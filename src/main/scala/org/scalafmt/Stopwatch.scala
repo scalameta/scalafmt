a1e7cbf3fc04bdd9bdccacd0e12e92a60731269e
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-01-28 16:42:43 +0100

    Create stopwatch class.

---
+++ src/main/scala/org/scalafmt/Stopwatch.scala
@@ -0,0 +1,14 @@
+package org.scalafmt
+
+import java.util.concurrent.TimeUnit
+
+// This must exist somewhere, couldn't find it though.
+class Stopwatch(val t1: Long) {
+  def elapsedMs = TimeUnit.MILLISECONDS.convert(elapsedNs, TimeUnit.NANOSECONDS)
+
+  def elapsedNs = System.nanoTime() - t1
+}
+
+object Stopwatch {
+  def apply() = new Stopwatch(System.nanoTime())
+}
