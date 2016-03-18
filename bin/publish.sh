8013ea257559c4a056c4635f7a4093c84af616d5
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-03-18 11:59:11 +0100

    Update publish script.

--- bin/publish.sh
+++ bin/publish.sh
@@ -3,7 +3,7 @@ set -e
 
 version=$(sed -n -e 's/.*val scalafmt = "\(.*\)"/\1/p' core/src/main/scala/org/scalafmt/Versions.scala)
 tag="v${version}"
-
+tarfile="cli/target/scalafmt.tar.gz"
 function assert-installed() {
   binary=$1
   command -v ${binary} >/dev/null 2>&1 || { echo >&2 "Missing dependency ${binary}, exiting."; exit 1; }
@@ -41,7 +41,6 @@ function maven-publish() {
 
 
 function update-github-release() {
-    tarfile="cli/target/scalafmt.tar.gz"
     rm -f ${tarfile}
     tar -cvzf ${tarfile} bin/scalafmt cli/target/scala-2.11/scalafmt.jar bin/configure
 
@@ -65,14 +64,13 @@ function update-github-release() {
 function update-homebrew-release() {
     # Update version
     brew_file="homebrew/scalafmt.rb"
-    sha_file="target/sha.tar.gz"
     sed -i '' -e "s/\(version \"\).*\"/\1${version}\"/" ${brew_file}
     # Update url
     tar_url=$(sed -n -e "s/  url \"\(.*\)\"/\1/p" ${brew_file})
     curl -L -o target/sha.tar.gz ${tar_url}
     sed -i '' -e "s/\(download\/\)[^\\]*\(\/scalafmt.tar.gz\)/\1${tag}\2/" homebrew/scalafmt.rb
     # Update sha
-    tar_sha256=$(shasum -a 256 ${sha_file} | cut -f1 -d" ")
+    tar_sha256=$(shasum -a 256 ${tarfile} | cut -f1 -d" ")
     sed -i '' -e "s/\(sha256 \"\).*\"/\1${tar_sha256}\"/" ${brew_file}
     cd homebrew
     git commit -am "Update to ${tag}"
@@ -87,5 +85,5 @@ push-tag
 maven-publish
 update-github-release
 update-homebrew-release
-./update-gh-pages.sh
+#./update-gh-pages.sh
 # TODO(olafur) update-intellij-plugin
