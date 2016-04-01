8feb9413fa2a5eb0ab1ff4b44403cb89874b548d
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-04-01 15:19:42 +0200

    Bump to 0.1.5.

--- core/src/main/scala/org/scalafmt/Versions.scala
+++ core/src/main/scala/org/scalafmt/Versions.scala
@@ -5,7 +5,7 @@ package org.scalafmt
   */
 object Versions {
   // Nightly, used in CLI, build.sbt, etc.
-  val nightly = "0.1.4"
+  val nightly = "0.1.5"
   // Stable, used in official user docs.
   val stable = "0.1.4"
 }
