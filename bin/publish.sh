f38cd4e33b367e19bebf2c6fad03ea271a1bf8cc
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-01 20:14:42 +0200

    Use nightly version in publish script.

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-version=$(sed -n -e 's/.*val stable = "\(.*\)"/\1/p' core/src/main/scala/org/scalafmt/Versions.scala)
+version=$(sed -n -e 's/.*val nightly = "\(.*\)"/\1/p' core/src/main/scala/org/scalafmt/Versions.scala)
 tag="v${version}"
 tarfile="cli/target/scalafmt.tar.gz"
 current_branch=$(git rev-parse --abbrev-ref HEAD)
