106b17ccfe26e5bcad216732167ca88388a3124e
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-21 13:50:54 +0200

    Infix 0 indents inside If/While, fixes #334

--- core/src/main/scala/org/scalafmt/util/TreeOps.scala
+++ core/src/main/scala/org/scalafmt/util/TreeOps.scala
@@ -422,11 +422,7 @@ object TreeOps {
   final def isTopLevelInfixApplication(child: Term.ApplyInfix): Boolean =
     child.parent match {
       case Some(parent: Term.ApplyInfix) => isTopLevelInfixApplication(parent)
-      case Some(_: Term.Block) => true
-      case Some(_: Source) => true
-//      case Some(e) =>
-//        logger.elem(log(e))
-//        false
+      case Some(_: Term.Block | _: Term.If | _: Term.While | _: Source) => true
       case _ => false
     }
 }
