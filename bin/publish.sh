b91d6fcd64147a9a74f2d7a47af612cc29bf3ef6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-23 15:01:51 +0200

    Update homebrew submodule.

--- bin/publish.sh
+++ bin/publish.sh
@@ -37,7 +37,7 @@ function confirm-release() {
 }
 
 function assemble-jar() {
-    sbt clean assembly
+    sbt clean cli/assembly
 }
 
 function push-tag() {
@@ -91,11 +91,11 @@ function update-homebrew-release() {
     git push origin master
 }
 
-assert-preconditions
-confirm-release
-#assemble-jar
-push-tag
-maven-publish
+#assert-preconditions
+#confirm-release
+##assemble-jar
+#push-tag
+#maven-publish
 update-github-release
 update-homebrew-release
 echo "Released ${tag}!"
