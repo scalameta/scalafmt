7cc6a93d725eb87c11521f4109660153c7d5af32
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-29 20:12:56 +0200

    Bump up version to 0.2.9

--- core/src/main/scala/org/scalafmt/Versions.scala
+++ core/src/main/scala/org/scalafmt/Versions.scala
@@ -5,7 +5,7 @@ package org.scalafmt
   */
 object Versions {
   // Nightly, used in CLI, build.sbt, etc.
-  val nightly = "0.2.8"
+  val nightly = "0.2.9"
   // Stable, used in official user docs.
-  val stable = "0.2.8"
+  val stable = "0.2.9"
 }
