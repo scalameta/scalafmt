f9fb4798e1f3593d2027f8528618ec6ec5800b7c
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-08-17 11:43:48 +0200

    Add drone file.

--- core/src/test/scala/org/scalafmt/stats/GitInfo.scala
+++ core/src/test/scala/org/scalafmt/stats/GitInfo.scala
@@ -1,35 +1,33 @@
 package org.scalafmt.stats
 
+import scala.util.Try
+
 // Name and email is anyways registered in the git log.
 
 case class GitInfo(name: String, email: String, branch: String, commit: String)
 
 object GitInfo {
-  import sys.process._
 
-  lazy val user = Seq("git", "config", "--get", "user.name").!!.trim
+  def exec(args: String*): String = {
+    import sys.process._
+    Try(args.!!.trim).getOrElse("ERROR")
+  }
 
-  lazy val email = Seq("git", "config", "--get", "user.email").!!.trim
+  lazy val user: String = exec("git", "config", "--get", "user.name")
+
+  lazy val email: String = exec("git", "config", "--get", "user.email")
 
   val travis: Option[GitInfo] = for {
-    isTravis <- sys.env.get("TRAVIS") if isTravis == "true"
-    isCi <- sys.env.get("CONTINUOUS_INTEGRATION") if isCi == "true"
-    commit <- sys.env.get("TRAVIS_COMMIT")
+    isCi <- sys.env.get("CI") if isCi == "true"
   } yield {
-    val branch = sys.env
-      .get("TRAVIS_PULL_REQUEST")
-      .withFilter(_ != "false")
-      .map(x => s"pull_request/$x")
-      .orElse(sys.env.get("TRAVIS_BRANCH"))
-      .getOrElse("unknown")
-    GitInfo("Travis", "Travis", branch, commit)
+    GitInfo("CI", "CI", currentBranch, currentCommit)
   }
 
   def apply(): GitInfo = travis.getOrElse {
     GitInfo(user, email, currentBranch, currentCommit)
   }
 
-  def currentCommit = Seq("git", "rev-parse", "HEAD").!!.trim
+  def currentCommit = exec("git", "rev-parse", "HEAD")
 
-  def currentBranch = Seq("git", "rev-parse", "--abbrev-ref", "HEAD").!!.trim
+  def currentBranch = exec("git", "rev-parse", "--abbrev-ref", "HEAD")
 }
