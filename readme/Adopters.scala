39f04f0880f66d2d9adcfaa88dc4616738551822
RomanIakovlev <iakovlev@live.de>
2016-09-08 18:09:41 +0200

    Update Adopters.scala

--- readme/Adopters.scala
+++ readme/Adopters.scala
@@ -16,6 +16,8 @@ object Adopters {
   val adopters = Seq[Adopter](
     Adopter("scalafmt",
             "http://scalafmt.org",
-            Some("Code formatter for Scala"))
-  )
+            Some("Code formatter for Scala")),
+    Adopter("HERE",
+            "http://here.com",
+            None)
 }
