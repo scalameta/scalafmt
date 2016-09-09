8c95524be27ca7568aa743fe62dfcd9bcc38c902
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-09 09:15:30 +0200

    Setup auto deploy on gh-pages

---
+++ bin/travisPublish.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ ${TRAVIS_SECURE_ENV_VARS} == "true" &&  && ${TRAVIS_BRANCH} == "master" ]]; then
+  echo "Publishing snapshot..."
+  # Assert that nightly is set to snapshot.
+  grep "nightly.*SNAPSHOT" core/src/main/scala/org/scalafmt/Versions.scala
+  # Save some useful information
+  sbt publish
+  ./bin/update-gh-pages.sh
+else
+  echo "Skipping publish"
+fi
--- bin/update-gh-pages.sh
+++ bin/update-gh-pages.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 set -e
-git checkout gh-pages
-git reset --hard master
-sbt "readme/run --validate-links"
-cp -r readme/target/scalatex/* .
-git add .
-git commit -m "Update ghpages."
-git push -f origin gh-pages
+
+echo "Updating gh-pages..."
+
+SUBDIR="gh-pages"
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
 git checkout master
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+sbt "readme/run --validate-links"
+
+rm -rf ${SUBDIR}
+git clone ${REPO} ${SUBDIR}
+cd ${SUBDIR}
+git checkout ${TARGET_BRANCH} || git checkout --orphan ${TARGET_BRANCH}
+cd ..
+cp -r readme/target/scalatex/* ${SUBDIR}
+
+cd ${SUBDIR}
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+if [ ${TRAVIS} = "true" ]; then
+  git config user.name "Travis CI"
+  git config user.email "olafurpg@gmail.com"
+  eval `ssh-agent -s`
+  ssh-add bin/travis_deploy
+fi
+
+git push origin gh-pages
+git checkout master
+
+echo "Done!"
