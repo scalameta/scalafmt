adf6c4a346f7712f3fc7ed89c3a0b8ef6c5bf0c7
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-14 14:40:40 +0100

    Attempt 2

--- core/src/main/scala/org/scalafmt/Versions.scala
+++ core/src/main/scala/org/scalafmt/Versions.scala
@@ -5,7 +5,7 @@ package org.scalafmt
   */
 object Versions {
   // Nightly, used in CLI, build.sbt, etc.
-  val nightly = "0.3.2-RC1"
+  val nightly = "0.3.2-RC3"
   // Stable, used in official user docs.
   val stable = "0.3.1"
   val scala = "2.11.8"
