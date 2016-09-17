892298eb38eec9590bef732478a7f2d8c0eadec8
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-17 21:35:12 +0200

    Move version to SNAPSHOT

--- core/src/main/scala/org/scalafmt/Versions.scala
+++ core/src/main/scala/org/scalafmt/Versions.scala
@@ -5,7 +5,7 @@ package org.scalafmt
   */
 object Versions {
   // Nightly, used in CLI, build.sbt, etc.
-  val nightly = "0.3.2"
+  val nightly = "0.3.2-SNAPSHOT"
   // Stable, used in official user docs.
   val stable = "0.3.2"
   val scala = "2.11.8"
