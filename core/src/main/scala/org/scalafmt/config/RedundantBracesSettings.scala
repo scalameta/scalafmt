eebf9080c4f4a839921cfd0956f1a08dec3b0ce2
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-25 17:30:29 +0200

    Remove unused setting in RedundantBracesSettings

--- core/src/main/scala/org/scalafmt/config/RedundantBracesSettings.scala
+++ core/src/main/scala/org/scalafmt/config/RedundantBracesSettings.scala
@@ -4,7 +4,6 @@ import metaconfig.ConfigReader
 
 @ConfigReader
 case class RedundantBracesSettings(
-    includeInferredType: Boolean = true,
     includeUnitMethods: Boolean = true,
     maxLines: Int = 100
 )
