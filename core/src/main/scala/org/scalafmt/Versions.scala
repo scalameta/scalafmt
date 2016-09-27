77fbf0df65591ea1cdee750defffe61862dc5501
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-27 19:12:42 +0200

    Update homebrew submodule.

--- core/src/main/scala/org/scalafmt/Versions.scala
+++ core/src/main/scala/org/scalafmt/Versions.scala
@@ -5,8 +5,8 @@ package org.scalafmt
   */
 object Versions {
   // Nightly, used in CLI, build.sbt, etc.
-  val nightly = "0.4.0"
+  val nightly = "0.4.1"
   // Stable, used in official user docs.
-  val stable = "0.4.0"
+  val stable = "0.4.1"
   val scala = "2.11.8"
 }
