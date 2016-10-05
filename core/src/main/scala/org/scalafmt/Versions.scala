89312a687867a28aaa56a4845ea3f99c6100def1
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-10-06 00:17:54 +0200

    Bump version to 0.4.4-SNAPSHOT

--- core/src/main/scala/org/scalafmt/Versions.scala
+++ core/src/main/scala/org/scalafmt/Versions.scala
@@ -5,7 +5,7 @@ package org.scalafmt
   */
 object Versions {
   // Nightly, used in CLI, build.sbt, etc.
-  val nightly = "0.4.3"
+  val nightly = "0.4.4-SNAPSHOT"
   // Stable, used in official user docs.
   val stable = "0.4.3"
   val scala = "2.11.8"
