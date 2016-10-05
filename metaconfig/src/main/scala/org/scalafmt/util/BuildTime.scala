e62ce13baaf2f0ffc7fda3e30138fdcb14b3eb61
Chris Birchall <chris.birchall@gmail.com>
2016-10-05 07:57:50 +0100

    Rewrite using quasiquotes

--- metaconfig/src/main/scala/org/scalafmt/util/BuildTime.scala
+++ metaconfig/src/main/scala/org/scalafmt/util/BuildTime.scala
@@ -8,8 +8,8 @@ class BuildTime extends scala.annotation.StaticAnnotation {
 
   inline def apply(defn: Any): Any = meta {
     defn match {
-      case defn @ Defn.Val(mods, pats, optType, rhs) if pats.size == 1 =>
-        defn.copy(rhs = Lit(System.currentTimeMillis()))
+      case q"..$mods val ${name: Pat.Var.Term}: $tpeopt = ???" =>
+        q"..$mods val $name: Long = ${System.currentTimeMillis()}"
       case _ =>
         abort("This annotation should be used as follows: `@BuildTime val buildTimeMs: Long = ???`")
     }
