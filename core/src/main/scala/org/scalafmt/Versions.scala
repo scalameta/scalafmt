b94e7215737926979ee2c2d2b9b67016445845ff
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-10-11 23:26:59 +0200

    Release 0.4.6 for Eugene

--- core/src/main/scala/org/scalafmt/Versions.scala
+++ core/src/main/scala/org/scalafmt/Versions.scala
@@ -5,8 +5,8 @@ package org.scalafmt
   */
 object Versions {
   // Nightly, used in CLI, build.sbt, etc.
-  val nightly = "0.4.5"
+  val nightly = "0.4.6"
   // Stable, used in official user docs.
-  val stable = "0.4.5"
+  val stable = "0.4.6"
   val scala = "2.11.8"
 }
