8d658e445ed8cb3707d097dd016f7ef944248a35
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-11-10 19:08:42 +0100

    Set version to 0.4.10

--- core/src/main/scala/org/scalafmt/Versions.scala
+++ core/src/main/scala/org/scalafmt/Versions.scala
@@ -5,8 +5,8 @@ package org.scalafmt
   */
 object Versions {
   // Nightly, used in CLI, build.sbt, etc.
-  val nightly = "0.4.9"
+  val nightly = "0.4.10"
   // Stable, used in official user docs.
-  val stable = "0.4.9"
+  val stable = "0.4.10"
   val scala = "2.11.8"
 }
