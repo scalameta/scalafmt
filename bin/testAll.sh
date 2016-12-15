8d868371c4e480fe30586999f89e7f1f670b0cec
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-12-15 17:25:34 +0100

    Revert "Don't run scripted tests on travis"
    This reverts commit 983b04372b76fd116c1b3767ac78012367412f44.

--- bin/testAll.sh
+++ bin/testAll.sh
@@ -4,6 +4,7 @@ set -e
 #sbt clean coverage test
 sbt clean test
 sbt "core/test:runMain org.scalafmt.FormatExperimentApp"
+sbt "; publishLocal ; scripted"
 ./bin/issue492.sh
 #sbt coverageAggregate
 
