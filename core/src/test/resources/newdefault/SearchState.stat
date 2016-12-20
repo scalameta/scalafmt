scalameta/scalafmt

commit 6bdfe5ceddfd24145fd2310df900caf7aefe396b

Automatically bin pack for argument lists of literals. (#629)
Fixes #431.

core/src/main/scala/org/scalafmt/config/BinPack.scala
@@ -1,10 +1,18 @@
 package org.scalafmt.config
 
+import scala.util.matching.Regex
+
 import metaconfig.ConfigReader
 
 @ConfigReader
 case class BinPack(
     callSite: Boolean = false,
     defnSite: Boolean = false,
-    parentConstructors: Boolean = false
-)
+    parentConstructors: Boolean = false,
+    literalArgumentLists: Boolean = true,
+    literalsInclude: Seq[String] = Seq(".*"),
+    literalsExclude: Seq[String] = Seq("String")
+) {
+  def literalsRegex: FilterMatcher =
+    FilterMatcher(literalsInclude, literalsExclude)
+}

core/src/main/scala/org/scalafmt/util/StyleMap.scala
@@ -1,28 +1,53 @@
 package org.scalafmt.util
 
+import scala.collection.mutable
+import scala.meta.Lit
+import scala.meta.Term
+import scala.meta.Tree
 import scala.meta.tokens.Token
 import scala.meta.tokens.Token.Comment
+import scala.meta.tokens.Token.LeftParen
+import scala.meta.tokens.Token.RightParen
 import scala.meta.tokens.Tokens
+import scala.util.matching.Regex
 
 import org.scalafmt.config.Config
+import org.scalafmt.config.FilterMatcher
 import org.scalafmt.config.ScalafmtConfig
 import org.scalafmt.internal.FormatToken
+import org.scalafmt.util.TokenOps.TokenHash
 
-class StyleMap(tokens: Array[FormatToken], init: ScalafmtConfig) {
+class StyleMap(tokens: Array[FormatToken],
+               init: ScalafmtConfig,
+               owners: Map[TokenHash, Tree],
+               matching: Map[TokenHash, Token]) {
+  import TokenOps.hash
+  val literalR: FilterMatcher = init.binPack.literalsRegex
   private val prefix = "\\s*scalafmt: ".r
+  val forcedBinPack: mutable.Set[Tree] = mutable.Set.empty
   val (isEmpty: Boolean, tok2style: Map[FormatToken, ScalafmtConfig]) = {
     var curr = init
     var empty = true
     val map = Map.newBuilder[FormatToken, ScalafmtConfig]
+    val disableBinPack = mutable.Set.empty[Token]
     tokens.foreach { tok =>
       tok.left match {
-        case x @ Comment(c) if prefix.findFirstIn(c).isDefined =>
+        case Comment(c) if prefix.findFirstIn(c).isDefined =>
           Config.fromHocon(c, Some("scalafmt")) match {
             case Right(style) =>
               empty = false
               curr = style
             case Left(e) => // TODO(olafur) report error via callback
           }
+        case open @ LeftParen()
+            if init.binPack.literalArgumentLists &&
+              isLiteralArgumentList(open) =>
+          forcedBinPack += owners(hash(open))
+          empty = false
+          curr = setBinPack(curr, callSite = true)
+          disableBinPack += matching(hash(open))
+        case close @ RightParen() if disableBinPack(close) =>
+          curr = setBinPack(curr, callSite = false)
         case _ =>
       }
       if (!empty) {
@@ -32,6 +57,28 @@ class StyleMap(tokens: Array[FormatToken], init: ScalafmtConfig) {
     }
     (empty, map.result())
   }
+
+  def setBinPack(curr: ScalafmtConfig, callSite: Boolean): ScalafmtConfig =
+    curr.copy(
+      binPack = curr.binPack.copy(
+        callSite = callSite
+      )
+    )
+
+  private def isLiteral(tree: Tree): Boolean = tree match {
+    case lit @ Lit(value: Any) =>
+      val strName =
+        if (lit.syntax.startsWith("0x")) "Byte"
+        else value.getClass.getName
+      literalR.matches(strName)
+    case _ => false
+  }
+
+  private def isLiteralArgumentList(open: LeftParen): Boolean =
+    owners(hash(open)) match {
+      case t: Term.Apply => t.args.forall(isLiteral)
+      case _ => false
+    }
 
   def at(token: FormatToken): ScalafmtConfig = {
     tok2style.getOrElse(token, init)

core/src/main/scala/org/scalafmt/internal/FormatOps.scala
@@ -42,8 +42,10 @@ class FormatOps(val tree: Tree, val initStyle: ScalafmtConfig) {
   val ownersMap = getOwners(tree)
   val statementStarts = getStatementStarts(tree)
   val dequeueSpots = getDequeueSpots(tree) ++ statementStarts.keys
-  val matchingParentheses = getMatchingParentheses(tree.tokens)
-  val styleMap = new StyleMap(tokens, initStyle)
+  val matchingParentheses: Map[TokenHash, Token] = getMatchingParentheses(
+    tree.tokens)
+  val styleMap =
+    new StyleMap(tokens, initStyle, ownersMap, matchingParentheses)
   private val vAlignDepthCache = mutable.Map.empty[Tree, Int]
   // Maps token to number of non-whitespace bytes before the token's position.
   private final val nonWhitespaceOffset: Map[Token, Int] = {
@@ -491,8 +493,13 @@ class FormatOps(val tree: Tree, val initStyle: ScalafmtConfig) {
     }
   }
 
-  def styleAt(tree: Tree): ScalafmtConfig =
-    styleMap.at(leftTok2tok.getOrElse(tree.tokens.head, tokens.head))
+  def styleAt(tree: Tree): ScalafmtConfig = {
+    val style =
+      styleMap.at(leftTok2tok.getOrElse(tree.tokens.head, tokens.head))
+    if (styleMap.forcedBinPack(tree)) // off-by-one
+      styleMap.setBinPack(style, callSite = true)
+    else style
+  }
 
   def getApplyIndent(leftOwner: Tree, isConfigStyle: Boolean = false): Num = {
     val style = styleAt(leftOwner)

core/src/main/scala/org/scalafmt/internal/Router.scala
@@ -396,7 +396,9 @@ class Router(formatOps: FormatOps) {
       case FormatToken(LeftParen() | LeftBracket(), right, between)
           if style.configStyleArguments &&
             (isDefnSite(leftOwner) || isCallSite(leftOwner)) &&
-            (opensConfigStyle(formatToken) || forceConfigStyle(leftOwner)) =>
+            (opensConfigStyle(formatToken) || {
+              forceConfigStyle(leftOwner) && !styleMap.forcedBinPack(leftOwner)
+            }) =>
         val open = formatToken.left
         val indent = getApplyIndent(leftOwner, isConfigStyle = true)
         val close = matchingParentheses(hash(open))

core/src/test/resources/binPack/LiteralList.stat
@@ -1,0 +1,201 @@
+binPack.literalArgumentLists = true
+<<< basic
+  val secret: List[Bit] = List(0,0,1,1,1,0,1,0,1,1,1,0,0,1,1,0,1,0,0,1,1,0,1,0,1,1,0,0,1,1,1,1,1,0,1,0,1,1,0,0,0,0,1,0,1,1,1,0,0,1,0,0,1,0,0,0,1,0,0,0,1,0,1)
+>>>
+val secret: List[Bit] = List(0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1,
+  0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1,
+  0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1)
+<<< basic 2
+  val secret: List[Bit] = List(0,0,1,1,1, // evil comment
+  0,1,0,1,1,1,0,0,1,1,0,1,0,0,1,1,0,1,0,1,1,0,0,1,1,1,1,1,0,1,0,1,1,0,0,0,0,1,0,1,1,1,0,0,1,0,0,1,0,0,0,1,0,0,0,1,0,1)
+>>>
+val secret: List[Bit] = List(0, 0, 1, 1, 1, // evil comment
+  0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1,
+  1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1,
+  0, 0, 0, 1, 0, 1)
+<<< native 1
+  private[this] lazy val charTypesFirst256 = Array[scala.Byte](15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 12, 24, 24, 24, 26, 24, 24, 24,
+    21, 22, 24, 25, 24, 20, 24, 24, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 24, 24, 25,
+    25, 25, 24, 24, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 21, 24, 22, 27, 23, 27, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 21, 25, 22, 25, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 12, 24, 26, 26, 26,
+    26, 28, 24, 27, 28, 5, 29, 25, 16, 28, 27, 28, 25, 11, 11, 27, 2, 24, 24,
+    27, 11, 5, 30, 11, 11, 11, 24, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 25, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 25, 2, 2, 2, 2, 2, 2,
+    2, 2)
+>>>
+private[this] lazy val charTypesFirst256 = Array[scala.Byte](15, 15, 15, 15,
+  15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+  15, 15, 15, 15, 15, 15, 15, 15, 15, 12, 24, 24, 24, 26, 24, 24, 24, 21, 22,
+  24, 25, 24, 20, 24, 24, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 24, 24, 25, 25, 25, 24,
+  24, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 21, 24, 22, 27, 23, 27, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 21, 25, 22, 25, 15, 15, 15, 15, 15, 15, 15, 15,
+  15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+  15, 15, 15, 15, 15, 15, 12, 24, 26, 26, 26, 26, 28, 24, 27, 28, 5, 29, 25,
+  16, 28, 27, 28, 25, 11, 11, 27, 2, 24, 24, 27, 11, 5, 30, 11, 11, 11, 24, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 25, 1, 1,
+  1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+  2, 2, 2, 25, 2, 2, 2, 2, 2, 2, 2, 2)
+<<< native 2
+  private val lengthByLeading: Array[Int] = Array(
+      // 10wwwwww
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      // 110yyyyy
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      // 1110zzzz
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      // 11110uuu
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      // > 11110111
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1 )
+>>>
+private val lengthByLeading: Array[Int] = Array(
+    // 10wwwwww
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, // 110yyyyy
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, // 1110zzzz
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, // 11110uuu
+    4, 4, 4, 4, 4, 4, 4, 4, // > 11110111
+    -1, -1, -1, -1, -1, -1, -1, -1)
+<<< byte
+  val secret: List[Bit] = List(0xB5,0xB5,0x2F,0x2F,0x2F,0xB5,0x2F,0xB5,0x2F,0x2F,0x2F,0xB5,0xB5,0x2F,0x2F,0xB5,0x2F,0xB5,0xB5,0x2F,0x2F,0xB5,0x2F,0xB5,0x2F,0x2F,0xB5,0xB5,0x2F,0x2F,0x2F,0x2F,0x2F,0xB5,0x2F,0xB5,0x2F,0x2F,0xB5,0xB5,0xB5,0xB5,0x2F,0xB5,0x2F,0x2F,0x2F,0xB5,0xB5,0x2F,0xB5,0xB5,0x2F,0xB5,0xB5,0xB5,0x2F,0xB5,0xB5,0xB5,0x2F,0xB5,1)
+>>>
+val secret: List[Bit] = List(0xB5, 0xB5, 0x2F, 0x2F, 0x2F, 0xB5, 0x2F, 0xB5,
+  0x2F, 0x2F, 0x2F, 0xB5, 0xB5, 0x2F, 0x2F, 0xB5, 0x2F, 0xB5, 0xB5, 0x2F, 0x2F,
+  0xB5, 0x2F, 0xB5, 0x2F, 0x2F, 0xB5, 0xB5, 0x2F, 0x2F, 0x2F, 0x2F, 0x2F, 0xB5,
+  0x2F, 0xB5, 0x2F, 0x2F, 0xB5, 0xB5, 0xB5, 0xB5, 0x2F, 0xB5, 0x2F, 0x2F, 0x2F,
+  0xB5, 0xB5, 0x2F, 0xB5, 0xB5, 0x2F, 0xB5, 0xB5, 0xB5, 0x2F, 0xB5, 0xB5, 0xB5,
+  0x2F, 0xB5, 1)
+

core/src/test/resources/binPack/LiteralListNoByte.stat
@@ -1,0 +1,68 @@
+binPack.literalArgumentLists = true
+binPack.literalsExclude = ["Byte"]
+<<< byte
+  val secret: List[Bit] = List(0xB5,0xB5,0x2F,0x2F,0x2F,0xB5,0x2F,0xB5,0x2F,0x2F,0x2F,0xB5,0xB5,0x2F,0x2F,0xB5,0x2F,0xB5,0xB5,0x2F,0x2F,0xB5,0x2F,0xB5,0x2F,0x2F,0xB5,0xB5,0x2F,0x2F,0x2F,0x2F,0x2F,0xB5,0x2F,0xB5,0x2F,0x2F,0xB5,0xB5,0xB5,0xB5,0x2F,0xB5,0x2F,0x2F,0x2F,0xB5,0xB5,0x2F,0xB5,0xB5,0x2F,0xB5,0xB5,0xB5,0x2F,0xB5,0xB5,0xB5,0x2F,0xB5,1)
+>>>
+val secret: List[Bit] = List(0xB5,
+                             0xB5,
+                             0x2F,
+                             0x2F,
+                             0x2F,
+                             0xB5,
+                             0x2F,
+                             0xB5,
+                             0x2F,
+                             0x2F,
+                             0x2F,
+                             0xB5,
+                             0xB5,
+                             0x2F,
+                             0x2F,
+                             0xB5,
+                             0x2F,
+                             0xB5,
+                             0xB5,
+                             0x2F,
+                             0x2F,
+                             0xB5,
+                             0x2F,
+                             0xB5,
+                             0x2F,
+                             0x2F,
+                             0xB5,
+                             0xB5,
+                             0x2F,
+                             0x2F,
+                             0x2F,
+                             0x2F,
+                             0x2F,
+                             0xB5,
+                             0x2F,
+                             0xB5,
+                             0x2F,
+                             0x2F,
+                             0xB5,
+                             0xB5,
+                             0xB5,
+                             0xB5,
+                             0x2F,
+                             0xB5,
+                             0x2F,
+                             0x2F,
+                             0x2F,
+                             0xB5,
+                             0xB5,
+                             0x2F,
+                             0xB5,
+                             0xB5,
+                             0x2F,
+                             0xB5,
+                             0xB5,
+                             0xB5,
+                             0x2F,
+                             0xB5,
+                             0xB5,
+                             0xB5,
+                             0x2F,
+                             0xB5,
+                             1)

commit 0319657bc0f593135d93abb6ae5df220b00da03f

Fix idempotency regression from #629

core/src/main/scala/org/scalafmt/config/BinPack.scala
@@ -1,7 +1,5 @@
 package org.scalafmt.config
 
-import scala.util.matching.Regex
-
 import metaconfig.ConfigReader
 
 /**
@@ -17,6 +15,7 @@ case class BinPack(
     lambdaParameter: Boolean = true,
     parentConstructors: Boolean = false,
     literalArgumentLists: Boolean = true,
+    literalsMinArgCount: Int = 5,
     literalsInclude: Seq[String] = Seq(".*"),
     literalsExclude: Seq[String] = Seq("String")
 ) {

core/src/main/scala/org/scalafmt/util/StyleMap.scala
@@ -76,7 +76,9 @@ class StyleMap(tokens: Array[FormatToken],
 
   private def isLiteralArgumentList(open: LeftParen): Boolean =
     owners(hash(open)) match {
-      case t: Term.Apply => t.args.forall(isLiteral)
+      case t: Term.Apply =>
+        t.args.length > init.binPack.literalsMinArgCount &&
+          t.args.forall(isLiteral)
       case _ => false
     }
 

core/src/test/resources/newdefault/SearchState.stat
@@ -61,3 +61,56 @@ val boss = system.actorOf(Props(new Actor {
       context.sender() ! TypedActor(context).typedActorOf(p)
   }
 }))
+<<< test setup idempotency
+
+{{
+      "to a HEAD request setting a custom Content-Type and Content-Length (default response entity)" in new TestSetup() {
+        ResponseRenderingContext(
+          requestMethod = HttpMethods.HEAD,
+          response =
+            HttpResponse(headers = List(Age(30)),
+                         entity =
+                           HttpEntity.Default(ContentTypes.`text/plain(UTF-8)`,
+                                              100,
+                                              Source.empty))
+        ) should renderTo(
+          """HTTP/1.1 200 OK
+              |Age: 30
+              |Server: akka-http/1.0.0
+              |Date: Thu, 25 Aug 2011 09:10:29 GMT
+              |Content-Type: text/plain; charset=UTF-8
+              |Content-Length: 100
+              |
+              |""",
+          close = false
+        )
+      }
+    }
+    }
+
+>>>
+{
+  {
+    "to a HEAD request setting a custom Content-Type and Content-Length (default response entity)" in new TestSetup() {
+      ResponseRenderingContext(
+        requestMethod = HttpMethods.HEAD,
+        response =
+          HttpResponse(headers = List(Age(30)),
+                       entity =
+                         HttpEntity.Default(ContentTypes.`text/plain(UTF-8)`,
+                                            100,
+                                            Source.empty))
+      ) should renderTo(
+        """HTTP/1.1 200 OK
+              |Age: 30
+              |Server: akka-http/1.0.0
+              |Date: Thu, 25 Aug 2011 09:10:29 GMT
+              |Content-Type: text/plain; charset=UTF-8
+              |Content-Length: 100
+              |
+              |""",
+        close = false
+      )
+    }
+  }
+}

core/src/test/scala/org/scalafmt/FormatExperiment.scala
@@ -1,5 +1,6 @@
 package org.scalafmt
 
+import scala.collection.JavaConverters._
 import scala.collection.JavaConversions._
 import scala.meta._
 import scala.util.Random
@@ -25,6 +26,7 @@ import org.scalafmt.util.FormatAssertions
 import org.scalafmt.util.ScalaFile
 import org.scalafmt.util.ScalaProjectsExperiment
 import org.scalafmt.util.ScalacParser
+import org.scalatest.FunSuiteLike
 
 trait FormatExperiment extends ScalaProjectsExperiment with FormatAssertions {
   override val verbose = false
@@ -118,7 +120,10 @@ object LinePerMsBenchmark extends FormatExperiment with App {
   Files.write(Paths.get("target", "macro.csv"), csvText.getBytes)
 }
 
-object FormatExperimentApp extends FormatExperiment with App {
+object FormatExperimentApp
+    extends FormatExperiment
+    with App
+    with FunSuiteLike {
   def valid(result: ExperimentResult): Boolean = result match {
     case _: Success | _: Timeout | _: Skipped | _: ParseErr |
         _: SearchStateExploded =>
@@ -138,8 +143,15 @@ object FormatExperimentApp extends FormatExperiment with App {
   }
   runExperiment(filesToRun)
   printResults()
+  val scalaResults = results.asScala
+  val idempotency = scalaResults.collect {
+    case UnknownFailure(_, e: IdempotencyViolated) => e
+  }
+  val searchStateExploded =
+    scalaResults.filter(_.isInstanceOf[SearchStateExploded])
+  assert(idempotency.length <= 13)
+  assert(searchStateExploded.length <= 16)
   val nonValidResults = results.filterNot(valid)
-  nonValidResults.foreach(println)
   if (nonValidResults.nonEmpty) {
     throw MegaTestFailed
   }