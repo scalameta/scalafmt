cd29b9a16271190218686b39f5186be4aff1849b
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-03-31 11:04:30 +0200

    Make cleanup_cache.sh safe to run.

--- bin/cleanup_cache.sh
+++ bin/cleanup_cache.sh
@@ -1,4 +1,4 @@
 set -eu
-find /drone/.sbt -name "*.lock" -type f -delete
-find /drone/.ivy2/cache -name "ivydata-*.properties" -type f -delete
+test -d /drone/.sbt && find /drone/.sbt -name "*.lock" -type f -delete
+test -d /drone/.ivy/cache && find /drone/.ivy2/cache -name "ivydata-*.properties" -type f -delete
 rm -rf /drone/.ivy2/local
