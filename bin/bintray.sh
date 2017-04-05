d1e345db21365c1d0cde91da4f2472f4aad03611
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-04-05 21:45:03 +0200

    Auto publish docs on merge to master, fixes #764 (#877)

--- bin/bintray.sh
+++ bin/bintray.sh
@@ -14,6 +14,7 @@ user = ${BINTRAY_USERNAME}
 password = ${BINTRAY_API_KEY}
 EOF
   /usr/bin/sbt ci-publish
+  ./bin/update-gh-pages.sh
 else
   echo "Skipping publish, branch=$DRONE_BRANCH test=$CI_TEST"
 fi
