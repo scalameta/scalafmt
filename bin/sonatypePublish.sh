1cfbba646a578d3ea6668fd0b0e19d9e7cbabd61
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-04-23 22:18:57 +0200

    updated site

--- bin/sonatypePublish.sh
+++
@@ -1,9 +0,0 @@
-set -e
-if [ $TRAVIS_SECURE_ENV_VARS = "true" ]; then
-  echo "Publishing snapshot..."
-  # Assert that nighly is set to snapshot
-  grep "nightly.*SNAPSHOT" core/target/scala-2.11/src_managed/main/sbt-buildinfo/Versions.scala
-  sbt publishSigned
-else
-  echo "Skipping publish"
-fi
--- project/Deps.scala
+++
@@ -1,6 +0,0 @@
-object Deps {
-  val scalameta = "1.6.0"
-  val scalatest = "3.0.1"
-  val scalariform = "0.1.8"
-  val coursier = "1.0.0-M15-1"
-}
