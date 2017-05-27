c959e73b43ff85f61d844c5f4b4230123e3b656f
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-05-27 23:35:30 +0200

    Exclude scalapb from runtime, fixes #944

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,13 +10,21 @@ object Dependencies {
   val scalatestV  = "3.0.1"
   val coursier    = "1.0.0-M15-5"
 
+  val scalapb = Def.setting {
+    ExclusionRule(
+      organization = "com.trueaccord.scalapb",
+      artifact = s"scalapb-runtime_${scalaBinaryVersion.value}"
+    )
+  }
+
   val paradise         = "org.scalameta"   % "paradise"     % paradiseV cross CrossVersion.full
   val scalametaTestkit = "org.scalameta"   %% "testkit"     % scalametaV
   val scalariform      = "org.scalariform" %% "scalariform" % "0.1.8"
 
   val scalatest          = Def.setting("org.scalatest" %%% "scalatest"                  % scalatestV)
-  val scalameta          = Def.setting("org.scalameta" %%% "scalameta"                  % scalametaV)
+  val scalameta          = Def.setting("org.scalameta" %%% "scalameta"                  % scalametaV excludeAll scalapb.value)
   val metaconfig         = Def.setting("com.geirsson"  %%% "metaconfig-core"            % metaconfigV)
   val metaconfigTypesafe = Def.setting("com.geirsson"  %%% "metaconfig-typesafe-config" % metaconfigV)
   val metaconfigHocon    = Def.setting("com.geirsson"  %%% "metaconfig-hocon"           % metaconfigV)
+
 }
