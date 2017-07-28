d12e4d4689be1dd1f09c83781db797d4b7289683
Gabriele Petronella <gabriele@buildo.io>
2017-07-28 12:29:34 +0200

    Add support for ranges

--- scalafmt-jsfacade/src/main/scala/org/scalafmt/jsfacade/JSFacade.scala
+++ scalafmt-jsfacade/src/main/scala/org/scalafmt/jsfacade/JSFacade.scala
@@ -1,6 +1,8 @@
 package org.scalafmt
 package jsfacade
 
+import scala.util.Try
+
 import scala.scalajs.js
 import js.JSConverters._
 import js.annotation._
@@ -9,11 +11,33 @@ import config.{ScalafmtConfig, Config}
 
 object JSFacade {
 
+  private[this] type Ranges = js.Array[js.Dictionary[Int]]
+
+  private[this] def toRanges(
+      ranges: js.UndefOr[Ranges]): Either[String, Set[Range]] =
+    Try {
+      ranges.toOption
+        .map(_.map(r => Range(r("start"), r("end"))).toSet)
+        .getOrElse(Set.empty)
+    }.toEither.left.map(_.toString)
+
   @JSExportTopLevel("format")
-  def format(input: String, hoconConfig: js.UndefOr[String]): String = {
+  def format(
+      input: String,
+      hoconConfig: js.UndefOr[String],
+      ranges: js.UndefOr[Ranges]): String = {
     (for {
-      config <- Config.fromHoconString(hoconConfig.getOrElse("")).toEither.left.map(_.toString)
-      formattedInput <- Scalafmt.format(input, config).toEither.left.map(_.toString)
+      config <- Config
+        .fromHoconString(hoconConfig.getOrElse(""))
+        .toEither
+        .left
+        .map(_.toString)
+      rr <- toRanges(ranges)
+      formattedInput <- Scalafmt
+        .format(input, config, rr)
+        .toEither
+        .left
+        .map(_.toString)
     } yield formattedInput).fold(
       errorMessage => throw js.JavaScriptException(errorMessage),
       formattedInput => formattedInput
