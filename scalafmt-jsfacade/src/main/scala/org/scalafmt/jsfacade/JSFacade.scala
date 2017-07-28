28c28f22771421b42d1db2d8fb1cd75874c9b80e
Gabriele Petronella <gabriele@buildo.io>
2017-07-28 10:56:48 +0200

    Add support for ScalafmtConfig

--- scalafmt-jsfacade/src/main/scala/org/scalafmt/jsfacade/JSFacade.scala
+++ scalafmt-jsfacade/src/main/scala/org/scalafmt/jsfacade/JSFacade.scala
@@ -5,11 +5,19 @@ import scala.scalajs.js
 import js.JSConverters._
 import js.annotation._
 
+import config.{ScalafmtConfig, Config}
+
 object JSFacade {
 
   @JSExportTopLevel("format")
-  def format(input: String): String = {
-    Scalafmt.format(input).get
+  def format(input: String, hoconConfig: js.UndefOr[String]): String = {
+    (for {
+      config <- Config.fromHoconString(hoconConfig.getOrElse("")).toEither.left.map(_.toString)
+      formattedInput <- Scalafmt.format(input, config).toEither.left.map(_.toString)
+    } yield formattedInput).fold(
+      errorMessage => throw js.JavaScriptException(errorMessage),
+      formattedInput => formattedInput
+    )
   }
 
 }
