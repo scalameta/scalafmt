afc44e64aff57986c12bb1d2b0216f18318b5138
Pedro Rodriguez <pedro@pjrt.co>
2017-08-17 00:09:57 -0400

    Fix #1000 #1010: Change lsTree to use lsFiles
    Turns out that's better

--- scalafmt-core/shared/src/main/scala/org/scalafmt/util/GitOps.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/util/GitOps.scala
@@ -16,42 +16,41 @@ object GitOps {
   def apply(): GitOps = new GitOpsImpl(AbsoluteFile.userDir)
 }
 
-class GitOpsImpl(workingDirectory: AbsoluteFile) extends GitOps {
+class GitOpsImpl(private[util] val workingDirectory: AbsoluteFile)
+    extends GitOps {
 
-  def exec(cmd: Seq[String]): String = {
-    val lastError = new StringBuilder
-    val swallowStderr = ProcessLogger(_ => Unit, err => lastError.append(err))
-    try {
-      sys.process
-        .Process(cmd, workingDirectory.jfile)
-        .!!(swallowStderr)
-        .trim
-    } catch {
-      case NonFatal(e) =>
-        throw new IllegalStateException(
-          s"Failed to run command ${cmd.mkString(" ")}. " +
-            s"Error: ${lastError.toString()}",
-          e)
+  private[util] def exec(cmd: Seq[String]): Try[Seq[String]] = {
+    val gitRes: Try[String] = Try {
+      val lastError = new StringBuilder
+      val swallowStderr = ProcessLogger(_ => Unit, err => lastError.append(err))
+      try {
+        sys.process
+          .Process(cmd, workingDirectory.jfile)
+          .!!(swallowStderr)
+          .trim
+      } catch {
+        case NonFatal(e) =>
+          throw new IllegalStateException(
+            s"Failed to run command ${cmd.mkString(" ")}. " +
+              s"Error: ${lastError.toString()}",
+            e)
+      }
     }
+
+    gitRes.map(_.lines.toSeq)
   }
 
   override def lsTree(dir: AbsoluteFile): Seq[AbsoluteFile] =
     rootDir.fold(Seq.empty[AbsoluteFile]) { rtDir =>
-      Try {
-        exec(
-          Seq(
-            "git",
-            "ls-tree",
-            "-r",
-            "HEAD",
-            "--name-only",
-            "--full-name",
-            dir.path
-          )
-          // DESNOTE(2017-06-02, pjrt): Since the git command above only returns
-          // files, no need to check if they are files
-        ).split(OsSpecific.lineSeparator).toSeq.map(f => rtDir / f)
-      }.getOrElse(Nil)
+      exec(
+        Seq(
+          "git",
+          "ls-files",
+          dir.path
+        )
+        // DESNOTE(2017-06-02, pjrt): Since the git command above only returns
+        // files, no need to check if they are files
+      ).toOption.toSeq.flatten.map(f => rtDir / f)
     }
 
   override def rootDir: Option[AbsoluteFile] = {
@@ -61,7 +60,7 @@ class GitOpsImpl(workingDirectory: AbsoluteFile) extends GitOps {
       "--show-toplevel"
     )
     for {
-      rootPath <- Try(exec(cmd)).toOption
+      Seq(rootPath) <- exec(cmd).toOption
       file <- AbsoluteFile.fromPath(rootPath)
       if file.jfile.isDirectory
     } yield file
@@ -79,7 +78,7 @@ class GitOpsImpl(workingDirectory: AbsoluteFile) extends GitOps {
     // files, no need to check if they are files
     for {
       root <- rootDir.toSeq
-      path <- Try(exec(cmd)).toOption.toSeq.flatMap(_.lines)
+      path <- exec(cmd).toOption.toSeq.flatten
     } yield AbsoluteFile.fromFile(new File(path), root)
   }
 }
