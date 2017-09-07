170e26c66979d4ddf443e07b0c74c063a4821ab9
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-09-07 23:22:23 +0200

    Add optInt.breaksInsideChains = true
    This option makes it possible to control line breaks around select
    chains. Fixes #988.

--- scalafmt-core/shared/src/main/scala/org/scalafmt/config/OptIn.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/config/OptIn.scala
@@ -8,6 +8,26 @@ import metaconfig._
   *                             opening ( and newline before closing ).
   *                             If true, preserves the newlines and keeps one
   *                             line per argument.
+  * @param breaksInsideChains
+  *  If true, then the user can opt out of line breaks
+  *  inside select chains.
+  *
+  *  {{{
+  *     // original
+  *     foo
+  *       .map(_ + 1).map(_ + 1)
+  *       .filter( > 2)
+  *     // if true
+  *     foo
+  *       .map(_ + 1).map(_ + 1)
+  *       .filter( > 2)
+  *     // if false
+  *     foo
+  *       .map(_ + 1)
+  *       .map(_ + 1)
+  *       .filter( > 2)
+  * }}}
+  *
   * @param breakChainOnFirstMethodDot
   *   If true, keeps the line break before a dot if it already exists.
   *   {{{
@@ -30,6 +50,7 @@ import metaconfig._
 @DeriveConfDecoder
 case class OptIn(
     configStyleArguments: Boolean = true,
+    breaksInsideChains: Boolean = false,
     breakChainOnFirstMethodDot: Boolean = true,
     selfAnnotationNewline: Boolean = true,
     annotationNewlines: Boolean = true
