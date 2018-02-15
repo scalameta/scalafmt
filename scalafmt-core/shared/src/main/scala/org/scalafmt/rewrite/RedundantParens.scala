1060098bf000ceb7c1a9b4bfa3b4460b5c09585c
Brandon Carl <bcarl@me.com>
2018-02-15 05:55:19 -0500

    Add balanced parenthesis check to RedundantParens - fixes #1111

--- scalafmt-core/shared/src/main/scala/org/scalafmt/rewrite/RedundantParens.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/rewrite/RedundantParens.scala
@@ -9,6 +9,17 @@ case object RedundantParens extends Rewrite {
   def isWrappedInParens(tokens: Tokens): Boolean =
     tokens.nonEmpty && tokens.head.is[LeftParen] && tokens.last.is[RightParen]
 
+  def hasBalancedParens(tokens: Tokens): Boolean =
+    tokens
+      .foldLeft((0, true)) {
+        case ((acc, false), _) => (acc, false)
+        case ((acc, true), token) if token.is[LeftParen] => (acc + 1, true)
+        case ((acc, true), token) if token.is[RightParen] =>
+          if (acc - 1 >= 0) { (acc - 1, true) } else { (acc, false) }
+        case ((acc, true), _) => (acc, true)
+      }
+      ._2
+
   override def rewrite(code: Tree, ctx: RewriteCtx): Seq[Patch] = {
     val builder = Seq.newBuilder[Patch]
     import ctx.dialect
@@ -23,8 +34,13 @@ case object RedundantParens extends Rewrite {
           val leftSegment: Tokens = tokens.slice(0, parensToRemoveCount)
           val rightSegment: Tokens =
             tokens.slice(tokens.length - parensToRemoveCount, tokens.length)
-          leftSegment.foreach(tok => builder += TokenPatch.Remove(tok))
-          rightSegment.foreach(tok => builder += TokenPatch.Remove(tok))
+          val innerSegment: Tokens = tokens.slice(
+            parensToRemoveCount,
+            tokens.length - parensToRemoveCount)
+          if (hasBalancedParens(innerSegment)) {
+            leftSegment.foreach(tok => builder += TokenPatch.Remove(tok))
+            rightSegment.foreach(tok => builder += TokenPatch.Remove(tok))
+          }
         }
     }
     builder.result()
