dd387b41b2bc0ffae5cb9955f4b9cea3d0a482e8
Brandon Carl <bcarl@fb.com>
2018-02-21 22:00:39 -0500

    Use ctx.isMatching() instead of checking balanced parens

--- scalafmt-core/shared/src/main/scala/org/scalafmt/rewrite/RedundantParens.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/rewrite/RedundantParens.scala
@@ -9,17 +9,6 @@ case object RedundantParens extends Rewrite {
   def isWrappedInParens(tokens: Tokens): Boolean =
     tokens.nonEmpty && tokens.head.is[LeftParen] && tokens.last.is[RightParen]
 
-  def hasBalancedParens(tokens: Tokens): Boolean =
-    tokens
-      .foldLeft((0, true)) {
-        case ((acc, false), _) => (acc, false)
-        case ((acc, true), token) if token.is[LeftParen] => (acc + 1, true)
-        case ((acc, true), token) if token.is[RightParen] =>
-          if (acc - 1 >= 0) { (acc - 1, true) } else { (acc, false) }
-        case ((acc, true), _) => (acc, true)
-      }
-      ._2
-
   override def rewrite(code: Tree, ctx: RewriteCtx): Seq[Patch] = {
     val builder = Seq.newBuilder[Patch]
     import ctx.dialect
@@ -34,12 +23,11 @@ case object RedundantParens extends Rewrite {
           val leftSegment: Tokens = tokens.slice(0, parensToRemoveCount)
           val rightSegment: Tokens =
             tokens.slice(tokens.length - parensToRemoveCount, tokens.length)
-          val innerSegment: Tokens = tokens.slice(
-            parensToRemoveCount,
-            tokens.length - parensToRemoveCount)
-          if (hasBalancedParens(innerSegment)) {
-            leftSegment.foreach(tok => builder += TokenPatch.Remove(tok))
-            rightSegment.foreach(tok => builder += TokenPatch.Remove(tok))
+          leftSegment.zip(rightSegment.reverse).foreach {
+            case (left, right) if ctx.isMatching(left, right) =>
+              builder += TokenPatch.Remove(left)
+              builder += TokenPatch.Remove(right)
+            case _ =>
           }
         }
     }
