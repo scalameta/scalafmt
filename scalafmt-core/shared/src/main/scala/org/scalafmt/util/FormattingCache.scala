695f92a4c8999d46797aa7643b40d18e3a870443
vovapolu <vovapolu@gmail.com>
2018-04-15 16:09:08 +0300

    private map for formatting cache

--- scalafmt-core/shared/src/main/scala/org/scalafmt/util/FormattingCache.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/util/FormattingCache.scala
@@ -5,7 +5,7 @@ import java.io.File
 import scala.collection.mutable
 
 object FormattingCache {
-  /*private*/ val lastFormatting = mutable.Map.empty[File, Long]
+  private val lastFormatting = mutable.Map.empty[File, Long]
 
   def outdatedFormatting(file: File): Boolean =
     lastFormatting.get(file).forall(_ < file.lastModified)
