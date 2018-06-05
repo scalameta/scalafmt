0b740f66f5aa9188fdee1c1272a1eb6cc328026b
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-06-05 20:17:22 +0200

    Use default value for isWindows check, fixes #1207

--- scalafmt-core/shared/src/main/scala/org/scalafmt/util/OsSpecific.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/util/OsSpecific.scala
@@ -3,8 +3,8 @@ package org.scalafmt.util
 /** Utils related to differences between various operating systems. */
 object OsSpecific {
   val isWindows: Boolean =
-    System.getProperty("os.name").toLowerCase().startsWith("windows")
   val lineSeparator: String = System.getProperty("line.separator")
+    System.getProperty("os.name", "unknown").toLowerCase().startsWith("windows")
 
   def fixSeparatorsInPathPattern(unixSpecificPattern: String): String =
     if (isWindows) unixSpecificPattern.replace("/", """\\""")
