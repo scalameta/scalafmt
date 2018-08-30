9e9c5df92f9c575d99a3aa02d20d22c9450e50a7
Santiago Saavedra <santiagosaavedra@gmail.com>
2018-08-30 19:48:01 +0200

    Ensure the Scala Ordering is used in the java PriorityQueue

--- scalafmt-core/jvm/src/main/scala/org/scalafmt/internal/PriorityQueue.scala
+++ scalafmt-core/jvm/src/main/scala/org/scalafmt/internal/PriorityQueue.scala
@@ -1,5 +1,7 @@
 package org.scalafmt.internal
 
+import java.util.Comparator
+
 /**
   * Minimal implementation of the PriorityQueue's functions needed.
   *
@@ -9,8 +11,11 @@ package org.scalafmt.internal
   *
   * @tparam T the values inside the queue
   */
-class PriorityQueue[T] {
-  private[this] val q = new java.util.PriorityQueue[T]
+class PriorityQueue[T <: Ordered[T]] {
+  private[this] val q =
+    new java.util.PriorityQueue[T](11, new Comparator[T] {
+      override def compare(t: T, t1: T): Int = t1.compare(t)
+    })
 
   def dequeueAll: Unit = q.clear()
 
@@ -20,9 +25,12 @@ class PriorityQueue[T] {
 
   def enqueue(x: T): Unit = q.add(x)
 
-  def +=(x: T): Unit = q.add(x)
+  def +=(x: T): PriorityQueue[T] = {
+    q.add(x)
+    this
+  }
 
-  def nonEmpty: Boolean = !q.isEmpty
+  def nonEmpty: Boolean = !isEmpty
 
   def isEmpty: Boolean = q.isEmpty
 
