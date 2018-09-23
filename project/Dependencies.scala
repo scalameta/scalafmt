62cda7339f9e8fc5da2368d2f42adc68a24d5e30
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-23 18:27:26 +0200

    Un-exclude scalapb from scalameta dependency on 2.11.
    Previously, we excluded the scalapb dependency to avoid un-unused bloat
    in Scalafmt. With a recent change in Scalameta some extension methods
    for the `Position` class now references ScalaPB generated `s.Range`
    definitions. Scala 2.12 is able to ignore the missing symbols since
    they're not needed at runtime, but Scala 2.11 complains about the
    missing symbols.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,22 @@ object Dependencies {
 
   val scalacheck = "org.scalacheck" %% "scalacheck" % scalacheckV
   val scalatest = Def.setting("org.scalatest" %%% "scalatest" % scalatestV)
-  val scalameta = Def.setting("org.scalameta" %%% "scalameta" % scalametaV excludeAll scalapb.value)
+  val scalameta = Def.setting {
+    scalaBinaryVersion.value match {
+      case "2.11" =>
+        // Scala 2.11.12 complains about missing scalapb symbols from the classpath:
+        // [error] /home/travis/build/scalameta/scalafmt/scalafmt-core/shared/src/main/scala/org/scalafmt/Error.scala:30:9: Symbol 'term <root>.scalapb' is missing from the classpath.
+        // [error] This symbol is required by 'class scala.meta.internal.semanticdb.Range'.
+        // [error] Make sure that term scalapb is in your classpath and check for conflicting dependencies with `-Ylog-classpath`.
+        // [error] A full rebuild may help if 'Range.class' was compiled against an incompatible version of <root>.
+        // [error]         pos.formatMessage(
+        // [error]         ^
+        // [error] one error found
+        "org.scalameta" %%% "scalameta" % scalametaV
+      case _ =>
+        "org.scalameta" %%% "scalameta" % scalametaV excludeAll scalapb.value
+    }
+  }
   val metaconfig = Def.setting("com.geirsson" %%% "metaconfig-core" % metaconfigV)
   val metaconfigTypesafe = Def.setting("com.geirsson" %%% "metaconfig-typesafe-config" % metaconfigV)
   val metaconfigHocon = Def.setting("com.geirsson" %%% "metaconfig-hocon" % metaconfigV)
