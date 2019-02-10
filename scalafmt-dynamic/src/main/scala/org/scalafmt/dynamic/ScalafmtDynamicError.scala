5cdd0c6c5574aad6320122bd4509cad07d596104
dmitrii.naumenko <dmitrii.naumenko@jetbrains.com>
2019-02-10 13:38:45 +0300

    #1344 refactor ScalafmtDynamic to sub-components
    1) Moved everything related to downloading process to
    ScalafmtDynamicDownloader
    2) Created errors ADT for all currently reported error types
    3) Split format method to two: one returns machine readable error from
    from errors ADT hierarchy, another one handles the error and reports
    user-readable message to provided reporter

---
+++ scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/ScalafmtDynamicError.scala
@@ -0,0 +1,21 @@
+package org.scalafmt.dynamic
+
+import java.nio.file.Path
+
+sealed trait ScalafmtDynamicError
+
+object ScalafmtDynamicError {
+  case class ConfigDoesNotExist(configPath: Path) extends ScalafmtDynamicError
+  case class ConfigMissingVersion(configPath: Path) extends ScalafmtDynamicError
+  case class ConfigParseError(configPath: Path, cause: ScalafmtConfigException)
+      extends ScalafmtDynamicError
+  case class CannotDownload(configPath: Path, version: String, cause: Option[Throwable])
+      extends ScalafmtDynamicError
+  case class UnknownError(message: String, cause: Option[Throwable])
+      extends ScalafmtDynamicError
+
+  object UnknownError {
+    def apply(cause: Throwable): UnknownError =
+      new UnknownError(cause.getMessage, Option(cause))
+  }
+}
