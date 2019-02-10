9d1f39a6630d71a47b6cbaad9758683bf8a16c42
dmitrii.naumenko <dmitrii.naumenko@jetbrains.com>
2019-02-10 12:57:39 +0300

    #1344 cosmetic: rearrange ConsoleScalafmtReporter methods declaration

--- scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/ConsoleScalafmtReporter.scala
+++ scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/ConsoleScalafmtReporter.scala
@@ -8,11 +8,6 @@ import org.scalafmt.interfaces.ScalafmtReporter
 object ConsoleScalafmtReporter extends ConsoleScalafmtReporter(System.err)
 
 class ConsoleScalafmtReporter(out: PrintStream) extends ScalafmtReporter {
-  override def downloadWriter(): PrintWriter = new PrintWriter(out)
-  def trimStacktrace(e: Throwable): Unit = ()
-  override def excluded(filename: Path): Unit = {
-    out.println(s"file excluded: $filename")
-  }
   override def error(file: Path, e: Throwable): Unit = {
     out.print(s"error: $file: ")
     trimStacktrace(e)
@@ -23,7 +18,15 @@ class ConsoleScalafmtReporter(out: PrintStream) extends ScalafmtReporter {
     out.println(s"error: $path: $message")
   }
 
+  override def excluded(filename: Path): Unit = {
+    out.println(s"file excluded: $filename")
+  }
+
   override def parsedConfig(config: Path, scalafmtVersion: String): Unit = {
     out.println(s"parsed config (v$scalafmtVersion): $config")
   }
+
+  override def downloadWriter(): PrintWriter = new PrintWriter(out)
+
+  protected def trimStacktrace(e: Throwable): Unit = ()
 }
