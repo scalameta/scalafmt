eab19037dcf1c6b8b4acdf5a99b80fe88be084fb
dmitrii.naumenko <dmitrii.naumenko@jetbrains.com>
2019-02-10 13:38:45 +0300

    #1344 refactor ScalafmtDynamic to sub-components
    1) Moved everything related to downloading process to
    ScalafmtDynamicDownloader
    2) Created errors ADT for all currently reported error types
    3) Split format method to two: one returns machine readable error from
    from errors ADT hierarchy, another one handles the error and reports
    user-readable message to provided reporter

--- scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/ScalafmtDynamic.scala
+++ scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/ScalafmtDynamic.scala
@@ -1,20 +1,13 @@
 package org.scalafmt.dynamic
 
-import com.geirsson.coursiersmall.CoursierSmall
-import com.geirsson.coursiersmall.Dependency
-import com.geirsson.coursiersmall.Repository
-import com.geirsson.coursiersmall.ResolutionException
-import com.geirsson.coursiersmall.Settings
-import com.typesafe.config.ConfigException
-import com.typesafe.config.ConfigFactory
-import java.lang.reflect.InvocationTargetException
-import java.net.URLClassLoader
-import java.nio.file.Files
-import java.nio.file.Path
+import java.nio.file.{Files, Path}
+
+import com.typesafe.config.{ConfigException, ConfigFactory}
 import org.scalafmt.interfaces._
+
 import scala.collection.concurrent.TrieMap
 import scala.collection.mutable
-import scala.concurrent.duration.Duration
+import scala.util.Try
 import scala.util.control.NonFatal
 
 final case class ScalafmtDynamic(
@@ -24,10 +17,7 @@ final case class ScalafmtDynamic(
     defaultVersion: String,
     fmts: mutable.Map[Path, ScalafmtReflect]
 ) extends Scalafmt {
-  override def clear(): Unit = {
-    fmts.values.foreach(_.classLoader.close())
-    fmts.clear()
-  }
+
   def this() = this(
     ConsoleScalafmtReporter,
     true,
@@ -36,66 +26,98 @@ final case class ScalafmtDynamic(
     TrieMap.empty[Path, ScalafmtReflect]
   )
 
-  override def withReporter(reporter: ScalafmtReporter): Scalafmt = {
-    copy(reporter = reporter)
-  }
-  override def withRespectProjectFilters(
-      respectExcludeFilters: Boolean
-  ): Scalafmt = {
-    copy(respectExcludeFilters = respectExcludeFilters)
-  }
-  override def withRespectVersion(respectVersion: Boolean): Scalafmt = {
-    copy(respectVersion = respectVersion)
+  private lazy val downloader =
+    new ScalafmtDynamicDownloader(respectVersion, respectExcludeFilters, reporter)
+
+  override def clear(): Unit = {
+    fmts.values.foreach(_.classLoader.close())
+    fmts.clear()
   }
 
-  override def withDefaultVersion(defaultVersion: String): Scalafmt = {
+  override def withReporter(reporter: ScalafmtReporter): Scalafmt =
+    copy(reporter = reporter)
+
+  override def withRespectProjectFilters(
+      respectExcludeFilters: Boolean): Scalafmt =
+    copy(respectExcludeFilters = respectExcludeFilters)
+
+  override def withRespectVersion(respectVersion: Boolean): Scalafmt =
+    copy(respectVersion = respectVersion)
+
+  override def withDefaultVersion(defaultVersion: String): Scalafmt =
     copy(defaultVersion = defaultVersion)
-  }
+
   override def format(config: Path, file: Path, code: String): String = {
-    def report(e: Throwable): Unit = e match {
-      case e: InvocationTargetException =>
-        report(e.getCause)
-      case _ =>
-        reporter.error(file, e)
+    formatDetailed(config, file, code) match {
+      case Right(codeFormatted) =>
+        codeFormatted
+      case Left(error) =>
+        reportError(file, error)
+        code
     }
-    def tryFormat(reflect: ScalafmtReflect): String = {
-      try {
-        reflect.format(file, code)
-      } catch {
+  }
+
+  private def reportError(file: Path, error: ScalafmtDynamicError): Unit = {
+    error match {
+      case ScalafmtDynamicError.ConfigParseError(configPath, cause) =>
+        reporter.error(configPath, cause.getMessage)
+      case ScalafmtDynamicError.ConfigDoesNotExist(configPath) =>
+        reporter.error(configPath, "file does not exist")
+      case ScalafmtDynamicError.ConfigMissingVersion(configPath) =>
+        reporter.missingVersion(configPath, defaultVersion)
+      case ScalafmtDynamicError.CannotDownload(configPath, version, cause) =>
+        val message = s"failed to resolve Scalafmt version '$version'"
+        cause match {
+          case Some(value) =>
+            reporter.error(configPath, ScalafmtException(message, value))
+          case None =>
+            reporter.error(configPath, message)
+        }
+      case ScalafmtDynamicError.UnknownError(_, Some(cause)) =>
+        reporter.error(file, cause)
+      case ScalafmtDynamicError.UnknownError(message, _) =>
+        reporter.error(file, message)
+    }
+  }
+
+  type FormatResult = Either[ScalafmtDynamicError, String]
+
+  def formatDetailed(config: Path, file: Path, code: String): FormatResult = {
+    def tryFormat(reflect: ScalafmtReflect): FormatResult = {
+      Try(reflect.format(file, code)).toEither.left.flatMap {
         case VersionMismatch(_, _) =>
           fmts.remove(config).foreach(_.classLoader.close())
-          format(config, file, code)
-        case ScalafmtConfigException(msg) =>
-          reporter.error(config, msg)
-          code
+          formatDetailed(config, file, code)
+        case ex: ScalafmtConfigException =>
+          Left(ScalafmtDynamicError.ConfigParseError(config, ex))
+        case ReflectionException(e) =>
+          Left(ScalafmtDynamicError.UnknownError(e))
         case NonFatal(e) =>
-          report(e)
-          code
+          Left(ScalafmtDynamicError.UnknownError(e))
       }
     }
-    fmts.get(config) match {
-      case Some(fmt) =>
-        tryFormat(fmt)
-      case None =>
-        if (!Files.exists(config)) {
-          reporter.error(config, "file does not exist")
-          code
-        } else {
-          readVersion(config) match {
-            case Some(version) =>
-              loadScalafmt(config, version) match {
-                case Some(fmt) =>
-                  fmts(config) = fmt
-                  tryFormat(fmt)
-                case None =>
-                  code
-              }
-            case None =>
-              reporter.missingVersion(config, defaultVersion)
-              code
-          }
+
+    def downloadAndFormat: FormatResult =
+      for {
+        _ <- Option(config).filter(conf => Files.exists(conf)).toRight {
+          ScalafmtDynamicError.ConfigDoesNotExist(config)
         }
-    }
+        version <- readVersion(config).toRight {
+          ScalafmtDynamicError.ConfigMissingVersion(config)
+        }
+        fmtReflect <- downloader.download(config, version).left.map { f =>
+          ScalafmtDynamicError.CannotDownload(config, f.version, f.cause)
+        }
+        codeFormatted <- {
+          fmts(config) = fmtReflect
+          tryFormat(fmtReflect)
+        }
+      } yield codeFormatted
+
+    fmts
+      .get(config)
+      .map(tryFormat)
+      .getOrElse(downloadAndFormat)
   }
 
   private def readVersion(config: Path): Option[String] = {
@@ -108,72 +130,4 @@ final case class ScalafmtDynamic(
         None
     }
   }
-
-  private def loadScalafmt(
-      config: Path,
-      version: String
-  ): Option[ScalafmtReflect] = {
-    def errorMessage = s"failed to resolve Scalafmt version '$version'"
-    try {
-      val scalaBinaryVersion =
-        if (version.startsWith("0.")) "2.11"
-        else "2.12"
-      val scalaVersion =
-        if (version.startsWith("0.")) BuildInfo.scala211
-        else BuildInfo.scala
-      val organization =
-        if (version.startsWith("1") || version.startsWith("0") || version == "2.0.0-RC1") {
-          "com.geirsson"
-        } else {
-          "org.scalameta"
-        }
-      val jars = CoursierSmall.fetch(
-        new Settings()
-          .withDependencies(
-            List(
-              new Dependency(
-                organization,
-                s"scalafmt-cli_$scalaBinaryVersion",
-                version
-              ),
-              new Dependency(
-                "org.scala-lang",
-                "scala-reflect",
-                scalaVersion
-              )
-            )
-          )
-          .withTtl(Some(Duration.Inf))
-          .withWriter(reporter.downloadWriter())
-          .withRepositories(
-            List(
-              Repository.MavenCentral,
-              Repository.Ivy2Local,
-              Repository.SonatypeReleases,
-              Repository.SonatypeSnapshots
-            )
-          )
-      )
-      val urls = jars.iterator.map(_.toUri.toURL).toArray
-      val classloader = new URLClassLoader(urls, null)
-      val fmt = ScalafmtReflect(
-        classloader,
-        config,
-        version,
-        respectVersion,
-        respectExcludeFilters,
-        reporter
-      )
-      fmts(config) = fmt
-      Some(fmt)
-    } catch {
-      case _: ResolutionException =>
-        reporter.error(config, errorMessage)
-        None
-      case NonFatal(e) =>
-        reporter.error(config, ScalafmtException(errorMessage, e))
-        None
-    }
-  }
-
 }
---
+++ scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/ScalafmtDynamicDownloader.scala
@@ -0,0 +1,91 @@
+package org.scalafmt.dynamic
+
+import java.net.URLClassLoader
+import java.nio.file.Path
+
+import com.geirsson.coursiersmall._
+import org.scalafmt.dynamic.ScalafmtDynamicDownloader.DownloadFailure
+import org.scalafmt.interfaces.ScalafmtReporter
+
+import scala.concurrent.duration.Duration
+import scala.util.Try
+import scala.util.control.NonFatal
+
+class ScalafmtDynamicDownloader(
+    respectVersion: Boolean,
+    respectExcludeFilters: Boolean,
+    reporter: ScalafmtReporter
+) {
+
+  type DownloadResult = Either[DownloadFailure, ScalafmtReflect]
+
+  def download(config: Path, version: String): DownloadResult = {
+    Try {
+      val jars: Seq[Path] = CoursierSmall.fetch(
+        new Settings()
+          .withDependencies(dependencies(version))
+          .withTtl(Some(Duration.Inf))
+          .withWriter(reporter.downloadWriter())
+          .withRepositories(
+            List(
+              Repository.MavenCentral,
+              Repository.Ivy2Local,
+              Repository.SonatypeReleases,
+              Repository.SonatypeSnapshots
+            )
+          )
+      )
+      val urls = jars.map(_.toUri.toURL).toArray
+      val classloader = new URLClassLoader(urls, null)
+      ScalafmtReflect(
+        classloader,
+        config,
+        version,
+        respectVersion,
+        respectExcludeFilters,
+        reporter
+      )
+    }.toEither.left.map {
+      case _: ResolutionException => DownloadFailure(version, None)
+      case NonFatal(e) => DownloadFailure(version, Some(e))
+    }
+  }
+
+  private def dependencies(version: String): List[Dependency] = {
+    List(
+      new Dependency(
+        organization(version),
+        s"scalafmt-cli_${scalaBinaryVersion(version)}",
+        version
+      ),
+      new Dependency(
+        "org.scala-lang",
+        "scala-reflect",
+        scalaVersion(version)
+      )
+    )
+  }
+
+  @inline
+  private def scalaBinaryVersion(version: String): String =
+    if (version.startsWith("0.")) "2.11"
+    else "2.12"
+
+  @inline
+  private def scalaVersion(version: String): String =
+    if (version.startsWith("0.")) BuildInfo.scala211
+    else BuildInfo.scala
+
+  @inline
+  private def organization(version: String): String =
+    if (version.startsWith("1") || version.startsWith("0") || version == "2.0.0-RC1") {
+      "com.geirsson"
+    } else {
+      "org.scalameta"
+    }
+
+}
+
+object ScalafmtDynamicDownloader {
+  case class DownloadFailure(version: String, cause: Option[Throwable])
+}
