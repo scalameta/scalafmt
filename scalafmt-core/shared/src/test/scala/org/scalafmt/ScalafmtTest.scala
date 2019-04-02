6124e3be092ebe58a61406d3a78c2d2be5f5fc58
Li Haoyi <haoyi.li@databricks.com>
2019-04-02 00:51:22 -0700

    .

--- scalafmt-core/shared/src/test/scala/org/scalafmt/ScalafmtTest.scala
+++ scalafmt-core/shared/src/test/scala/org/scalafmt/ScalafmtTest.scala
@@ -51,5 +51,98 @@ class ScalafmtTest extends org.scalatest.FunSuite {
        |""".stripMargin,
     config.ScalafmtConfig.default40
   )
+  check(
+    """|object Foo {
+       |  function(
+       |    a &&
+       |    b
+       |  )
+       |}
+       |""".stripMargin,
+    """|object Foo {
+       |  function(
+       |    a &&
+       |    b
+       |  )
+       |}
+       |""".stripMargin,
+    config.ScalafmtConfig.default40.copy(maxColumn = 10, unindentTopLevelOperators = true)
+  )
+  check(
+    """|object Foo1 {
+       |  function(
+       |    a &&
+       |    b
+       |  )
+       |}
+       |""".stripMargin,
+    """|object Foo1 {
+       |  function(
+       |    a &&
+       |    b
+       |  )
+       |}
+       |""".stripMargin,
+    config.ScalafmtConfig.default40.copy(maxColumn = 10)
+  )
+
+  check(
+    """|object Foo {
+       |  val x =
+       |    a +
+       |    b
+       |}
+       |""".stripMargin,
+    """|object Foo {
+       |  val x =
+       |    a +
+       |    b
+       |}
+       |""".stripMargin,
+    config.ScalafmtConfig.default40.copy(maxColumn = 10, unindentTopLevelOperators = true)
+  )
+  check(
+    """|object Foo2 {
+       |  val x =
+       |    a +
+       |    b
+       |}
+       |""".stripMargin,
+    """|object Foo2 {
+       |  val x =
+       |    a +
+       |    b
+       |}
+       |""".stripMargin,
+    config.ScalafmtConfig.default40.copy(maxColumn = 10)
+  )
+
+  check(
+    """|object Foo {
+       |  a +
+       |  b
+       |}
+       |""".stripMargin,
+    """|object Foo {
+       |  a +
+       |  b
+       |}
+       |""".stripMargin,
+    config.ScalafmtConfig.default40.copy(maxColumn = 6, unindentTopLevelOperators = true)
+  )
+
+  check(
+    """|object Foo3 {
+       |  a +
+       |  b
+       |}
+       |""".stripMargin,
+    """|object Foo3 {
+       |  a +
+       |  b
+       |}
+       |""".stripMargin,
+    config.ScalafmtConfig.default40.copy(maxColumn = 6)
+  )
 
 }
