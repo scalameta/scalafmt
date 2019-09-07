b6647cd06276d1b1c67afc185dcc273a22107e68
tanishiking24 <rikiriki1238@gmail.com>
2019-09-08 01:37:38 +0900

    Update scalameta to 4.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
   val metaconfigV = "0.8.3"
-  val scalametaV = "4.2.0"
+  val scalametaV = "4.2.3"
   val scalatestV = "3.2.0-SNAP10"
   val scalacheckV = "1.13.5"
   val coursier = "1.0.3"
