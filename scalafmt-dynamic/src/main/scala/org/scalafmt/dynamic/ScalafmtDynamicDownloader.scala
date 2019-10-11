a5c102fd5d0d5dca92e7ea76e24262eba0af293f
Henri Cook <henri.cook@linklaters.com>
2019-10-11 13:29:27 +0100

    Review comments

--- scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/ScalafmtDynamicDownloader.scala
+++ scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/ScalafmtDynamicDownloader.scala
@@ -1,20 +1,14 @@
 package org.scalafmt.dynamic
 
-import java.io.PrintWriter
+import java.io.OutputStreamWriter
 import java.net.URL
-import java.nio.file.Path
 
 import coursierapi._
-
-import scala.collection.JavaConverters._
 import org.scalafmt.dynamic.ScalafmtDynamicDownloader._
 
+import scala.collection.JavaConverters._
 import scala.concurrent.duration.Duration
 import scala.util.Try
-import java.io.OutputStream
-import java.io.PrintStream
-import java.io.OutputStreamWriter
-import scala.concurrent.JavaConversions._
 
 class ScalafmtDynamicDownloader(
     downloadProgressWriter: OutputStreamWriter,
