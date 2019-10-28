35605f2f0116913e29727c2e36e558400b20a017
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-10-28 10:53:52 +0000

    Remove `avoidEmptyLinesAroundBlock` option
    Followup from https://github.com/scalameta/scalafmt/pull/1431. This
    setting is already covered by the `edition` option.

--- scalafmt-core/shared/src/main/scala/org/scalafmt/config/ScalafmtConfig.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/config/ScalafmtConfig.scala
@@ -194,7 +194,7 @@ case class ScalafmtConfig(
   )
 
   val avoidEmptyLinesAroundBlock: Boolean =
-    newlines.avoidEmptyLinesAroundBlock.getOrElse(edition > Edition(2019, 10))
+    edition > Edition(2019, 10)
 }
 
 object ScalafmtConfig {
