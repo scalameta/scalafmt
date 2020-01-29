d7241839dc3a294e1abd9c1e36f403ff0d6300fe
tanishiking24 <rikiriki1238@gmail.com>
2020-01-30 00:32:58 +0900

    Enable to download and run snapshot version from dynamic

--- scalafmt-dynamic/src/test/scala/tests/ScalafmtVersionSuite.scala
+++ scalafmt-dynamic/src/test/scala/tests/ScalafmtVersionSuite.scala
@@ -6,20 +6,55 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class ScalafmtVersionSuite extends AnyFunSuite {
   test("parse valid versions") {
-    assert(ScalafmtVersion.parse("2.0.0") == Right(ScalafmtVersion(2, 0, 0, 0)))
-    assert(ScalafmtVersion.parse("0.1.3") == Right(ScalafmtVersion(0, 1, 3, 0)))
-    assert(
-      ScalafmtVersion.parse("2.0.0-RC4") == Right(ScalafmtVersion(2, 0, 0, 4))
-    )
-    assert(ScalafmtVersion.parse("2.1.1") == Right(ScalafmtVersion(2, 1, 1, 0)))
     assert(
       ScalafmtVersion
-        .parse("2.2.3-SNAPSHOT") == Right(ScalafmtVersion(2, 2, 3, 0))
+        .parse("2.0.0") == Right(ScalafmtVersion(2, 0, 0, 0, false))
+    )
+    assert(
+      ScalafmtVersion
+        .parse("0.1.3") == Right(ScalafmtVersion(0, 1, 3, 0, false))
+    )
+    assert(
+      ScalafmtVersion.parse("2.0.0-RC4") == Right(
+        ScalafmtVersion(2, 0, 0, 4, false)
+      )
+    )
+    assert(
+      ScalafmtVersion
+        .parse("2.1.1") == Right(ScalafmtVersion(2, 1, 1, 0, false))
+    )
+    assert(
+      ScalafmtVersion
+        .parse("2.2.3-SNAPSHOT") == Right(ScalafmtVersion(2, 2, 3, 0, true))
     )
     assert(
       ScalafmtVersion.parse("2.0.0-RC1-SNAPSHOT") == Right(
-        ScalafmtVersion(2, 0, 0, 1)
+        ScalafmtVersion(2, 0, 0, 1, true)
       )
+    )
+    assert(
+      ScalafmtVersion.parse("2.2.2-SNAPSHOT") == Right(
+        ScalafmtVersion(2, 2, 2, 0, true)
+      )
+    )
+  }
+
+  test("toString") {
+    assert(ScalafmtVersion.parse("2.2.2-RC2").right.get.toString == "2.2.2-RC2")
+    assert(ScalafmtVersion.parse("2.2.2").right.get.toString == "2.2.2")
+    assert(
+      ScalafmtVersion
+        .parse("2.2.2-SNAPSHOT")
+        .right
+        .get
+        .toString == "2.2.2-SNAPSHOT"
+    )
+    assert(
+      ScalafmtVersion
+        .parse("2.2.2-RC2-SNAPSHOT")
+        .right
+        .get
+        .toString == "2.2.2-RC2-SNAPSHOT"
     )
   }
 
@@ -68,16 +103,38 @@ class ScalafmtVersionSuite extends AnyFunSuite {
   }
 
   test("order versions") {
-    assert(ScalafmtVersion(2, 0, 0, 0) > ScalafmtVersion(1, 5, 1, 0))
-    assert(ScalafmtVersion(2, 0, 0, 0) > ScalafmtVersion(2, 0, 0, 1))
-    assert(ScalafmtVersion(2, 0, 0, 4) > ScalafmtVersion(1, 9, 9, 9))
-    assert(ScalafmtVersion(0, 1, 2, 0) > ScalafmtVersion(0, 1, 1, 0))
-    assert(ScalafmtVersion(0, 2, 2, 0) > ScalafmtVersion(0, 1, 2, 0))
-    assert(ScalafmtVersion(2, 0, 0, 2) < ScalafmtVersion(2, 0, 0, 4))
-    assert(ScalafmtVersion(2, 0, 0, 2) < ScalafmtVersion(2, 0, 0, 0))
-    assert(ScalafmtVersion(0, 1, 2, 0) < ScalafmtVersion(1, 0, 0, 0))
-    assert(ScalafmtVersion(0, 1, 8, 0) < ScalafmtVersion(0, 2, 2, 0))
-    assert(!(ScalafmtVersion(2, 0, 0, 0) < ScalafmtVersion(1, 5, 1, 0)))
-    assert(!(ScalafmtVersion(0, 1, 8, 0) > ScalafmtVersion(0, 2, 2, 0)))
+    assert(
+      ScalafmtVersion(2, 0, 0, 0, false) > ScalafmtVersion(1, 5, 1, 0, false)
+    )
+    assert(
+      ScalafmtVersion(2, 0, 0, 0, false) > ScalafmtVersion(2, 0, 0, 1, false)
+    )
+    assert(
+      ScalafmtVersion(2, 0, 0, 4, false) > ScalafmtVersion(1, 9, 9, 9, false)
+    )
+    assert(
+      ScalafmtVersion(0, 1, 2, 0, false) > ScalafmtVersion(0, 1, 1, 0, false)
+    )
+    assert(
+      ScalafmtVersion(0, 2, 2, 0, false) > ScalafmtVersion(0, 1, 2, 0, false)
+    )
+    assert(
+      ScalafmtVersion(2, 0, 0, 2, false) < ScalafmtVersion(2, 0, 0, 4, false)
+    )
+    assert(
+      ScalafmtVersion(2, 0, 0, 2, false) < ScalafmtVersion(2, 0, 0, 0, false)
+    )
+    assert(
+      ScalafmtVersion(0, 1, 2, 0, false) < ScalafmtVersion(1, 0, 0, 0, false)
+    )
+    assert(
+      ScalafmtVersion(0, 1, 8, 0, false) < ScalafmtVersion(0, 2, 2, 0, false)
+    )
+    assert(
+      !(ScalafmtVersion(2, 0, 0, 0, false) < ScalafmtVersion(1, 5, 1, 0, false))
+    )
+    assert(
+      !(ScalafmtVersion(0, 1, 8, 0, false) > ScalafmtVersion(0, 2, 2, 0, false))
+    )
   }
 }
