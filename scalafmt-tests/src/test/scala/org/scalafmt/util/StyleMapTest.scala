4a9871e9326b5dbf0d5d117482540b66b27a8903
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2020-02-04 07:48:27 -0800

    Add failing test for align.tokens.add

--- scalafmt-tests/src/test/scala/org/scalafmt/util/StyleMapTest.scala
+++ scalafmt-tests/src/test/scala/org/scalafmt/util/StyleMapTest.scala
@@ -2,7 +2,6 @@ package org.scalafmt.util
 
 import scala.meta._
 
-import org.scalafmt.config.ScalafmtRunner
 import org.scalafmt.config.ScalafmtConfig
 import org.scalafmt.internal.FormatOps
 import org.scalatest.funsuite.AnyFunSuite
@@ -28,6 +27,33 @@ class StyleMapTest extends AnyFunSuite {
         .maxColumn == 100
     )
     assert(m.styleMap.at(m.tokens.last).maxColumn == 110)
+  }
+
+  test("align.tokens.add") {
+    val code =
+      """object a {
+        |  // scalafmt: { align.tokens.add = ["="] }
+        |  println(1)
+        |}
+      """.stripMargin.parse[Source].get
+    val formatOps = new FormatOps(code, ScalafmtConfig.defaultWithAlign)
+    val headToken = formatOps.tokens.head
+    val printlnToken = formatOps.tokens.find(_.left.syntax == "println").get
+
+    val defaultEquals = "(Enumerator.Val|Defn.(Va(l|r)|Def|Type))"
+    val overrideEquals = ".*"
+
+    val headConfig = formatOps.styleMap.at(headToken)
+    val printlnConfig = formatOps.styleMap.at(printlnToken)
+    val newFormatOps = new FormatOps(code, printlnConfig)
+
+    val newHeadConfig = newFormatOps.styleMap.at(headToken)
+    assert(headConfig.alignMap("=").regex == defaultEquals)
+    assert(newHeadConfig.alignMap("=").regex == overrideEquals)
+
+    val newPrintlnConfig = newFormatOps.styleMap.at(printlnToken)
+    assert(printlnConfig.alignMap("=").regex == overrideEquals)
+    assert(newPrintlnConfig.alignMap("=").regex == defaultEquals) // this is wrong
   }
 
 }
