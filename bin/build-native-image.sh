4347c7b73cf1eac3d558eec836df42ad9365c1fc
Mikhail Chugunkov <poslegm@gmail.com>
2020-02-19 19:16:43 +0300

    Fixed version in build script

--- bin/build-native-image.sh
+++ bin/build-native-image.sh
@@ -1,6 +1,6 @@
 set -eux
 # NOTE(olafur): for some reason `jabba use ...` doesn't seem to work on GH Actions
-export JAVA_HOME=$(jabba which --home graal-custom@19.3)
+export JAVA_HOME=$(jabba which --home graal-custom@20.0)
 export PATH=$JAVA_HOME/bin:$PATH
 echo $JAVA_HOME
 which gu
