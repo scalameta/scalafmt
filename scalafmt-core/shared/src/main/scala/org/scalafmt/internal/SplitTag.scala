90eb8c9dceac37ca3a83df1545ee90bbd5f97d14
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2020-02-23 11:23:04 -0800

    Split: switch ignoreIf to tag, allows more control
    Rather than enabling a split using a simple boolean flag, let's allow
    a split to be enabled in some cases.

---
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/internal/SplitTag.scala
@@ -0,0 +1,25 @@
+package org.scalafmt.internal
+
+sealed abstract class SplitTag {
+
+  def activateOnly(splits: Seq[Split]): Seq[Split]
+
+}
+
+object SplitTag {
+
+  abstract class Base extends SplitTag {
+    override final def activateOnly(splits: Seq[Split]): Seq[Split] = splits
+  }
+
+  abstract class Custom extends SplitTag {
+    override final def activateOnly(splits: Seq[Split]): Seq[Split] =
+      splits.flatMap { s =>
+        if (s.isTaggedFor(this)) Some(s.copy(tag = SplitTag.Active)) else None
+      }
+  }
+
+  case object Active extends Base
+  case object Ignored extends Base
+
+}
