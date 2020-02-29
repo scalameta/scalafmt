0a1830ea759e03472bf8b79e8336a6537f50752e
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2020-02-29 13:57:32 -0800

    DanglingParentheses: extend the encoder/decoder
    We will add another field in the next commits, and the current logic is
    not ready for it. Also, stop encoding using the boolean shortcut used
    when both flags were the same, as it will no longer be appropriate with
    additional fields.

--- scalafmt-core/shared/src/main/scala/org/scalafmt/config/DanglingParentheses.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/config/DanglingParentheses.scala
@@ -9,26 +9,25 @@ case class DanglingParentheses(
   val decoder: ConfDecoder[DanglingParentheses] =
     generic.deriveDecoder(this).noTypos
 }
+
 object DanglingParentheses {
-  val default = DanglingParentheses(true, true)
+
+  private val shortcutTrue = DanglingParentheses(true, true)
+  private val shortcutFalse = DanglingParentheses(false, false)
+
+  val default = shortcutTrue
+
   implicit lazy val surface: generic.Surface[DanglingParentheses] =
     generic.deriveSurface
 
   implicit val decoder: ConfDecoder[DanglingParentheses] =
     ConfDecoder.instance[DanglingParentheses] {
-      case Conf.Bool(true) => Configured.Ok(DanglingParentheses(true, true))
-      case Conf.Bool(false) => Configured.Ok(DanglingParentheses(false, false))
+      case Conf.Bool(true) => Configured.Ok(shortcutTrue)
+      case Conf.Bool(false) => Configured.Ok(shortcutFalse)
       case els => default.decoder.read(els)
     }
 
   implicit val encoder: ConfEncoder[DanglingParentheses] =
-    ConfEncoder.instance[DanglingParentheses] {
-      case DanglingParentheses(true, true) => Conf.Bool(true)
-      case DanglingParentheses(true, false) =>
-        Conf.Obj("callSite" -> Conf.Bool(true), "defnSite" -> Conf.Bool(false))
-      case DanglingParentheses(false, true) =>
-        Conf.Obj("callSite" -> Conf.Bool(false), "defnSite" -> Conf.Bool(true))
-      case DanglingParentheses(false, false) => Conf.Bool(false)
+    generic.deriveEncoder
 
-    }
 }
