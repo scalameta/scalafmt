2a68a654ec8e46aec1edda5c17504e5138c5d8c2
Brad Solomon <bsolomon@protonmail.com>
2020-04-15 20:44:40 -0400

    [squash-me] comment out push

--- bin/docker-publish.sh
+++ bin/docker-publish.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+# TODO: we could also possibly use
+# https://github.com/marketplace/actions/build-and-push-docker-images
+# in place of this.
+
 set -ex
+
+if [[ -z "$DOCKER_USERNAME" ]]; then
+    echo "Error: Missing DOCKER_USERNAME" >&2
+    exit 1
+fi
+
+if [[ -z "$DOCKER_PASSWORD" ]]; then
+    echo "Error: Missing DOCKER_PASSWORD" >&2
+    exit 1
+fi
 
 # CD over to docker/
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -10,19 +24,21 @@ cd "$DOCKERDIR" || exit 1
 # Extract version, build and push
 SCALAFMT_VERSION=$(git describe --tags | sed 's/^v//')
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build \
+docker image build \
     --no-cache \
     --build-arg SCALAFMT_VERSION="$SCALAFMT_VERSION" \
     --progress=plain \
     --tag "scalameta/$DOCKER_USERNAME:$SCALAFMT_VERSION" \
     --tag "scalameta/$DOCKER_USERNAME:latest" \
     .
-docker push "scalameta/$DOCKER_USERNAME:$SCALAFMT_VERSION"
-# Don't tag releaes candidate (RC) versions with 'latest'
+docker image ls --format 'table {{.Repository}}\t{{.Tag}}\t{{.ID}}\t{{.Size}}'
+# docker image push "scalameta/$DOCKER_USERNAME:$SCALAFMT_VERSION"
+# Don't tag release candidate (RC) versions with 'latest'
 case "$SCALAFMT_VERSION" in
     *RC*)
         ;;
     *)
-        docker push "scalameta/$DOCKER_USERNAME:latest"
+        echo "PUSHING LATEST"
+        # docker image push "scalameta/$DOCKER_USERNAME:latest"
         ;;
 esac
