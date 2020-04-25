c8df78734588b065e638a0fefb311a5c9df0996c
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2020-04-24 20:21:47 -0700

    ScalafmtRunner: refactor file matching

--- scalafmt-core/shared/src/main/scala/org/scalafmt/config/FilterMatcher.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/config/FilterMatcher.scala
@@ -5,15 +5,13 @@ import scala.util.matching.Regex
 import org.scalafmt.util.AbsoluteFile
 
 case class FilterMatcher(include: Regex, exclude: Regex) {
-  def matches(file: AbsoluteFile): Boolean = matches(file.path)
+  def matchesFile(file: AbsoluteFile): Boolean = matches(file.path)
   def matches(input: String): Boolean =
     include.findFirstIn(input).isDefined &&
       exclude.findFirstIn(input).isEmpty
 }
 
 object FilterMatcher {
-  val matchEverything = new FilterMatcher(".*".r, mkRegexp(Nil))
-
   def mkRegexp(filters: Seq[String], strict: Boolean = false): Regex =
     filters match {
       case Nil => "$a".r // will never match anything
