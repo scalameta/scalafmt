aa443fcfc0c667059ed524cd2b80356d71ec9223
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2020-05-12 10:55:56 -0700

    mdoc DefaultsModifier: allow multiple defaults

--- scalafmt-docs/src/main/scala/docs/DefaultsModifier.scala
+++ scalafmt-docs/src/main/scala/docs/DefaultsModifier.scala
@@ -24,14 +24,29 @@ class DefaultsModifier extends StringModifier {
         Conf.printHocon(ScalafmtConfig.default)
       "```\n" + result + "\n```"
     } else {
-      val path = code.text.split("\\.").toList
-      val down = path.foldLeft(default.dynamic)(_ selectDynamic _)
-      down.asConf match {
-        case Configured.Ok(value) =>
-          "Default: `" + code.text.trim + " = " + value.toString + "`\n"
-        case Configured.NotOk(e) =>
-          reporter.error(Position.Range(code, 0, 0), e.toString())
-          "fail"
+      def getDefaultValue(param: String): String = {
+        val path = param.split("\\.").toList
+        val down = path.foldLeft(default.dynamic)(_ selectDynamic _)
+        down.asConf match {
+          case Configured.Ok(value) =>
+            value.toString()
+          case Configured.NotOk(e) =>
+            reporter.error(Position.Range(code, 0, 0), e.toString())
+            "<fail>"
+        }
+      }
+
+      val params = code.text.split("\\s+")
+      if (params.length == 1) {
+        val param = params(0)
+        s"Default: `$param = ${getDefaultValue(param)}`\n"
+      } else {
+        val defaults = params.map { param =>
+          s"$param = ${getDefaultValue(param)}"
+        }
+        ScalafmtModifier.mdConfigCodeBlock(
+          defaults.mkString("# Defaults\n", "\n", "")
+        )
       }
     }
   }
