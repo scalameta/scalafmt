79bf9bbdc8d5a76026ae10b4539d0bed8446b996
Nick Pavlov <gurinderu@gmail.com>
2020-06-21 18:32:19 +0300

    Add build with musl libc

--- bin/build-native-image.sh
+++ bin/build-native-image.sh
@@ -1,8 +1,5 @@
 set -eux
 # NOTE(olafur): for some reason `jabba use ...` doesn't seem to work on GH Actions
-export JAVA_HOME=$(jabba which --home graal-custom@20.0)
-export PATH=$JAVA_HOME/bin:$PATH
-echo $JAVA_HOME
 which gu
 gu install native-image
 sbt cli/graalvm-native-image:packageBin
