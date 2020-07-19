e7738441b24fc95ac29707e958198c961807b5f7
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2020-07-19 08:24:50 -0700

    FormatOps: afterCurlyLambda=preserve for [un]fold

--- scalafmt-core/shared/src/main/scala/org/scalafmt/internal/FormatOps.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/internal/FormatOps.scala
@@ -1225,7 +1225,12 @@ class FormatOps(
         (space, Newline)
       case Newlines.AfterCurlyLambdaParams.always => (false, Newline2x)
       case Newlines.AfterCurlyLambdaParams.preserve =>
-        (newlines == 0, if (newlines >= 2) Newline2x else Newline)
+        val space = style.newlines.source match {
+          case Newlines.fold => true
+          case Newlines.unfold => false
+          case _ => newlines == 0
+        }
+        (space, if (newlines >= 2) Newline2x else Newline)
     }
 
   def getNoSplit(
