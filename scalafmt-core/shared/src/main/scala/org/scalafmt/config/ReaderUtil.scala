2d45d35cbb85ee0412e9121df491e13107031946
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2020-07-19 00:14:14 -0700

    ReaderUtil: remove redundant oneOfIgnoreBackticks

--- scalafmt-core/shared/src/main/scala/org/scalafmt/config/ReaderUtil.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/config/ReaderUtil.scala
@@ -6,27 +6,16 @@ import metaconfig._
 import metaconfig.Configured._
 
 object ReaderUtil {
-  // Poor mans coproduct reader
-  def oneOf[T: ClassTag](options: sourcecode.Text[T]*): ConfCodec[T] =
-    oneOfImpl((lowerCase _).compose(lowerCaseNoBackticks), options)
 
-  def oneOfIgnoreBackticks[T: ClassTag](
-      options: sourcecode.Text[T]*
-  ): ConfCodec[T] =
-    oneOfImpl(lowerCaseNoBackticks, options)
-
-  private def lowerCase(s: String): String = s.toLowerCase
   private def lowerCaseNoBackticks(s: String): String =
     s.toLowerCase().replace("`", "")
 
-  private def oneOfImpl[T: ClassTag](
-      sanitize: String => String,
-      options: Seq[sourcecode.Text[T]]
-  ): ConfCodec[T] = {
-    val m = options.map(x => sanitize(x.source) -> x.value).toMap
+  // Poor mans coproduct reader
+  def oneOf[T: ClassTag](options: sourcecode.Text[T]*): ConfCodec[T] = {
+    val m = options.map(x => lowerCaseNoBackticks(x.source) -> x.value).toMap
     val decoder = ConfDecoder.instance[T] {
       case Conf.Str(x) =>
-        m.get(sanitize(x)) match {
+        m.get(lowerCaseNoBackticks(x)) match {
           case Some(y) =>
             Ok(y)
           case None =>
@@ -44,4 +33,5 @@ object ReaderUtil {
     }
     ConfCodec.EncoderDecoderToCodec(encoder, decoder)
   }
+
 }
