93edeeed4be938748fb4a758f7abde91c6af6b17
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-04-10 20:42:47 -0700

    Rewrite Scala3: add flags to drop optional delims
    Add tests with explicit braces and/or parens, to rewrite them later.

---
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/config/RewriteScala3Settings.scala
@@ -0,0 +1,43 @@
+package org.scalafmt.config
+
+import org.scalafmt.config.RewriteScala3Settings._
+
+import metaconfig._
+
+case class RewriteScala3Settings(
+    convertToNewSyntax: Boolean = false,
+    removeOptionalBraces: RemoveOptionalBraces = RemoveOptionalBraces.no
+) extends Decodable[RewriteScala3Settings] {
+  override protected[config] def baseDecoder: ConfDecoder[T] =
+    RewriteScala3Settings.codec
+}
+
+object RewriteScala3Settings {
+  implicit val surface: generic.Surface[RewriteScala3Settings] =
+    generic.deriveSurface
+  implicit val codec: ConfCodec[RewriteScala3Settings] =
+    generic.deriveCodec(new RewriteScala3Settings)
+
+  implicit val preset: PartialFunction[Conf, RewriteScala3Settings] = {
+    case Conf.Bool(true) =>
+      new RewriteScala3Settings(true, RemoveOptionalBraces.yes)
+    case Conf.Bool(false) => new RewriteScala3Settings
+  }
+
+  sealed abstract class RemoveOptionalBraces
+
+  object RemoveOptionalBraces {
+
+    implicit val codec: ConfCodec[RemoveOptionalBraces] = ReaderUtil
+      .oneOfCustom[RemoveOptionalBraces](yes, no, oldSyntaxToo) {
+        case Conf.Bool(true) => Configured.Ok(yes)
+        case Conf.Bool(false) => Configured.Ok(no)
+      }
+
+    case object no extends RemoveOptionalBraces
+    case object yes extends RemoveOptionalBraces
+    case object oldSyntaxToo extends RemoveOptionalBraces
+
+  }
+
+}
