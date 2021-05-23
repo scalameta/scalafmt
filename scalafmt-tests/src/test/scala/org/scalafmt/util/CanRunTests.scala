937202b4dd86a1ba9796feb87b3834965bd01db1
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-05-23 12:27:33 -0700

    HasTests: include the parser in DiffTest

--- scalafmt-tests/src/test/scala/org/scalafmt/util/CanRunTests.scala
+++ scalafmt-tests/src/test/scala/org/scalafmt/util/CanRunTests.scala
@@ -1,15 +1,12 @@
 package org.scalafmt.util
 
 import munit.FunSuite
-import scala.meta.Tree
-import scala.meta.parsers.Parse
 import munit.Location
 import scala.meta.parsers.ParseException
 
 trait CanRunTests extends FunSuite with HasTests {
-  def runTest(run: (DiffTest, Parse[_ <: Tree]) => Unit)(t: DiffTest): Unit = {
+  def runTest(run: DiffTest => Unit)(t: DiffTest): Unit = {
     implicit val loc: Location = t.loc
-    val filename = loc.path
     val paddedName = f"${t.fullName}%-70s|"
 
     if (ignore(t)) {
@@ -18,19 +15,15 @@ trait CanRunTests extends FunSuite with HasTests {
       test(paddedName.ignore) {}
     } else {
       test(paddedName) {
-        filename2parse(filename) match {
-          case Some(parse) =>
-            try {
-              run.apply(t, parse)
-            } catch {
-              case FormatException(e: ParseException, code) =>
-                fail(
-                  "test does not parse\n" +
-                    parseException2Message(e, code),
-                  e
-                )
-            }
-          case None => fail(s"Found no parse for filename $filename")
+        try {
+          run(t)
+        } catch {
+          case FormatException(e: ParseException, code) =>
+            fail(
+              "test does not parse\n" +
+                parseException2Message(e, code),
+              e
+            )
         }
       }
     }
