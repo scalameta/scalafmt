40c352486d4673f7fa2e9835599414365e159485
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-07-20 16:03:23 -0700

    Imports: add tests before the implementation

--- scalafmt-core/shared/src/main/scala/org/scalafmt/rewrite/Imports.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/rewrite/Imports.scala
@@ -1,10 +1,25 @@
 package org.scalafmt.rewrite
 
+import org.scalafmt.config.ReaderUtil
 import org.scalafmt.config.RewriteSettings
 
 import metaconfig._
 
 object Imports {
+
+  case class Settings(
+      sort: Sort = Sort.none,
+      expand: Boolean = false,
+      groups: Seq[Seq[String]] = Nil
+  ) {
+    def nonEmpty: Boolean = sort.ne(Sort.none) || expand || groups.nonEmpty
+  }
+
+  object Settings {
+    implicit val surface: generic.Surface[Settings] = generic.deriveSurface
+    implicit val codec: ConfCodecEx[Settings] =
+      generic.deriveCodecEx(new Settings).noTypos
+  }
 
   private val allImportRules: Set[Rewrite] =
     Set(ExpandImportSelectors, SortImports, AsciiSortImports)
@@ -15,6 +30,36 @@ object Imports {
       val msg = importRules.mkString("Incompatible rewrites: ", ", ", "")
       Configured.NotOk(ConfError.message(msg))
     } else Configured.Ok(obj)
+  }
+
+  sealed abstract class Sort {
+    // TODO
+  }
+
+  private object Sort {
+
+    implicit val reader: ConfCodecEx[Sort] =
+      ReaderUtil.oneOf[Sort](none, ascii, original, scalastyle)
+
+    case object none extends Sort {
+      // TODO
+    }
+
+    abstract class SortBase extends Sort {
+      // TODO
+    }
+
+    case object ascii extends SortBase {
+      // TODO
+    }
+
+    case object original extends SortBase {
+      // TODO
+    }
+
+    case object scalastyle extends SortBase {
+      // TODO
+    }
   }
 
 }
