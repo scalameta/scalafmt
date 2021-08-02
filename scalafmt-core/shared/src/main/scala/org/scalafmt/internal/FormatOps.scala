d3d7e8b0c7c8b4280859b208c9eb8515c3f4fc4e
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-08-02 08:02:01 -1000

    BinPack: implement logic for unsafeXxxSite=Oneline

--- scalafmt-core/shared/src/main/scala/org/scalafmt/internal/FormatOps.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/internal/FormatOps.scala
@@ -369,6 +369,15 @@ class FormatOps(
       getOneArgPerLineSplitsAfterComma(right, splits)
   }
 
+  def splitOneArgPerLineAfterCommaOnBreak(comma: T): Policy =
+    delayedBreakPolicyBefore(comma) {
+      Policy.after(comma) {
+        case Decision(t @ FormatToken(`comma`, right, _), splits)
+            if !right.is[T.Comment] || t.hasBreak =>
+          getOneArgPerLineSplitsAfterComma(right, splits)
+      }
+    }
+
   private def getOneArgPerLineSplitsAfterComma(r: Token, s: Seq[Split]) =
     if (r.is[T.LeftBrace]) SplitTag.OneArgPerLine.activateOnly(s)
     else Decision.onlyNewlineSplits(s)
