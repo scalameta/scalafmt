92467d2da90f059cf9734841b055353a34146e76
Scala Steward <me@scala-steward.org>
2021-08-24 22:43:01 +0200

    Update munit to 0.7.28

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val scalametaV  = "4.4.27"
   val scalacheckV = "1.15.4"
   val coursier    = "1.0.3"
-  val munitV      = "0.7.27"
+  val munitV      = "0.7.28"
 
   val scalapb = Def.setting {
     ExclusionRule(
