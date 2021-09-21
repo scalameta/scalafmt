a04ed811092f6c09c46477923d29dadfa5d95fcf
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-09-21 12:06:19 -0700

    TrailingCommas: move as nested class inside object

--- scalafmt-core/shared/src/main/scala/org/scalafmt/config/TrailingCommas.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/config/TrailingCommas.scala
@@ -2,34 +2,36 @@ package org.scalafmt.config
 
 import metaconfig._
 
-/** ADT representing trailing commas settings
-  *
-  * When [[org.scalafmt.config.TrailingCommas.always]] is selected, trailing
-  * commas are added everywhere a newline is followed by a right parens, brace
-  * or bracket.
-  *
-  * When [[org.scalafmt.config.TrailingCommas.multiple]] is selected, trailing
-  * commas are only added if there's more than one non-repeated argument.
-  *
-  * When [[org.scalafmt.config.TrailingCommas.never]] is selected, trailing
-  * commas are removed whenever they appear.
-  *
-  * When [[org.scalafmt.config.TrailingCommas.keep]] is selected, existing
-  * trailing commas will be preserved, and no new ones will be added.
-  */
-sealed abstract class TrailingCommas
-
 object TrailingCommas {
 
-  implicit val reader: ConfCodecEx[TrailingCommas] =
-    ReaderUtil.oneOfCustom[TrailingCommas](always, never, keep, multiple) {
-      case Conf.Str(str) if str.equalsIgnoreCase("preserve") =>
-        Configured.Ok(keep)
-    }
+  /** ADT representing trailing commas settings
+    *
+    * When [[org.scalafmt.config.TrailingCommas.always]] is selected, trailing
+    * commas are added everywhere a newline is followed by a right parens, brace
+    * or bracket.
+    *
+    * When [[org.scalafmt.config.TrailingCommas.multiple]] is selected, trailing
+    * commas are only added if there's more than one non-repeated argument.
+    *
+    * When [[org.scalafmt.config.TrailingCommas.never]] is selected, trailing
+    * commas are removed whenever they appear.
+    *
+    * When [[org.scalafmt.config.TrailingCommas.keep]] is selected, existing
+    * trailing commas will be preserved, and no new ones will be added.
+    */
+  sealed abstract class Style
 
-  case object always extends TrailingCommas
-  case object never extends TrailingCommas
-  case object keep extends TrailingCommas
-  case object multiple extends TrailingCommas
+  object Style {
+    implicit val codec: ConfCodecEx[Style] =
+      ReaderUtil.oneOfCustom[Style](always, never, keep, multiple) {
+        case Conf.Str(str) if str.equalsIgnoreCase("preserve") =>
+          Configured.Ok(keep)
+      }
+  }
+
+  case object always extends Style
+  case object never extends Style
+  case object keep extends Style
+  case object multiple extends Style
 
 }
