94c3fccdd4ac6ed60901356704a4b89fda35f405
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-11-21 07:35:40 -0800

    Interfaces: create session in main Scalafmt trait

--- scalafmt-interfaces/src/main/java/org/scalafmt/interfaces/Scalafmt.java
+++ scalafmt-interfaces/src/main/java/org/scalafmt/interfaces/Scalafmt.java
@@ -115,4 +115,12 @@ public interface Scalafmt {
         );
     }
 
+
+    /**
+     * Create a ScalafmtSession to format a batch of files using fixed configuration.
+     * @param config location of the configuration file
+     * @return a new session instance
+     */
+    ScalafmtSession createSession(Path config);
+
 }
