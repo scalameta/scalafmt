d24151865090ad96a1a8ec03ec60616c26580416
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-11-25 10:01:32 -0800

    ReflectionException: add flatten() method

--- scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/exceptions/ReflectionException.scala
+++ scalafmt-dynamic/src/main/scala/org/scalafmt/dynamic/exceptions/ReflectionException.scala
@@ -3,9 +3,10 @@ package org.scalafmt.dynamic.exceptions
 import java.lang.reflect.InvocationTargetException
 
 object ReflectionException {
-  def unapply(e: Throwable): Option[Throwable] =
-    e match {
-      case e: InvocationTargetException => Some(e.getCause)
-      case _ => Some(e)
-    }
+  def unapply(e: Throwable): Option[Throwable] = Some(flatten(e))
+
+  def flatten(e: Throwable): Throwable = e match {
+    case e: InvocationTargetException => e.getCause
+    case _ => e
+  }
 }
