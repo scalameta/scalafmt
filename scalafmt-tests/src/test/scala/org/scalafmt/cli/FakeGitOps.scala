fd3bd974c89681741b0543128e4a3ef16935575e
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-11-27 08:58:29 -0800

    GitOps: remove workingDirectory field

--- scalafmt-tests/src/test/scala/org/scalafmt/cli/FakeGitOps.scala
+++ scalafmt-tests/src/test/scala/org/scalafmt/cli/FakeGitOps.scala
@@ -3,7 +3,6 @@ package org.scalafmt.cli
 import org.scalafmt.sysops.{AbsoluteFile, GitOps}
 
 class FakeGitOps(root: AbsoluteFile) extends GitOps {
-  override val workingDirectory: AbsoluteFile = root
   override def lsTree(dir: AbsoluteFile*): Seq[AbsoluteFile] =
     dir.flatMap(_.listFiles)
   override def rootDir: Option[AbsoluteFile] = Some(root)
