96a6a5da09511eeae95c3bde88682907aeab78d4
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-11-30 11:07:21 -0800

    ScalafmtSession: add formatOrError()

--- scalafmt-interfaces/src/main/java/org/scalafmt/interfaces/ScalafmtSession.java
+++ scalafmt-interfaces/src/main/java/org/scalafmt/interfaces/ScalafmtSession.java
@@ -20,6 +20,16 @@ public interface ScalafmtSession {
 
 
     /**
+     * Format a single file with the given configuration.
+     *
+     * @param file relative or absolute path to the file being formatted. Used only for the path
+     *             name, the file does not have to exist on disk.
+     * @param code the text contents to format.
+     * @return the formatted contents if formatting was successful, otherwise an error.
+     */
+    ScalafmtResult formatOrError(Path file, String code);
+
+    /**
      * Whether the path matches the 'project.{excludeFilters,includeFilters}' setting.
      * @param file path to match
      * @return true if the path matched the filters
