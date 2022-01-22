scalameta/scalafmt

commit 8229ff9045a3a33ef30f4b5972494568960fc27f

Reformat with scalafmt 3.0.0

readme/src/main/scala/org/scalafmt/readme/Readme.scala
@@ -43,9 +43,7 @@ object Readme {
 
   /** repl session, inspired by tut.
     *
-    * Example: code="1 + 1" returns
-    * "scala> 1 + 1
-    * res0: Int = 2"
+    * Example: code="1 + 1" returns "scala> 1 + 1 res0: Int = 2"
     */
   def repl(code: String) = {
     import scala.meta._
@@ -232,16 +230,16 @@ object Readme {
       )
     )
 
-  /** This looks way too hacky. But can't seem to find a typeclass
-    * that ought to "encode" the ``ModKey`` enum.
+  /** This looks way too hacky. But can't seem to find a typeclass that ought to
+    * "encode" the ``ModKey`` enum.
     *
-    * Additionally, a Vector of Strings is simply concatenated, hence
-    * the extra .mkString.
+    * Additionally, a Vector of Strings is simply concatenated, hence the extra
+    * .mkString.
     * {{{
-    *    [error]  found   : Vector[org.scalafmt.config.SortSettings.ModKey]
-    *    [error]  required: scalatags.Text.Modifier
-    *    [error]     (which expands to)  scalatags.generic.Modifier[scalatags.text.Builder]
-    *    [error]           @code{rewrite.sortModifiers.order} = @rewriteSortModifiers.rewrite.sortModifiers.order
+    *     [error]  found   : Vector[org.scalafmt.config.SortSettings.ModKey]
+    *     [error]  required: scalatags.Text.Modifier
+    *     [error]     (which expands to)  scalatags.generic.Modifier[scalatags.text.Builder]
+    *     [error]           @code{rewrite.sortModifiers.order} = @rewriteSortModifiers.rewrite.sortModifiers.order
     * }}}
     */
   val rewriteSortModifiersDefaultString =

scalafmt-cli/src/main/scala/org/scalafmt/cli/CliOptions.scala
@@ -17,15 +17,15 @@ object CliOptions {
 
   /** Tries to read configuration from
     *
-    * 1. .scalafmt.conf in root dir of current git repo
-    *     IF the following setting is enabled: project.git = true
-    * 2. .scalafmt.conf from init.common.workingDirectory
+    *   1. .scalafmt.conf in root dir of current git repo IF the following
+    *      setting is enabled: project.git = true 2. .scalafmt.conf from
+    *      init.common.workingDirectory
     *
     * I am happy to add alternative fallback methods for other VCS.
     *
     * WARNING. Throws an exception if the .scalafmt.conf error exists but
-    * contains an error. Why? Because this method is only supposed to be
-    * called directly from main.
+    * contains an error. Why? Because this method is only supposed to be called
+    * directly from main.
     */
   def auto(parsed: CliOptions): CliOptions = {
     val auxOut =
@@ -109,8 +109,8 @@ case class CliOptions(
 
   val writeMode: WriteMode = writeModeOpt.getOrElse(WriteMode.Override)
 
-  /** Create a temporary file that contains configuration string specified by `--config-str`.
-    * This temporary file will be passed to `scalafmt-dynamic`.
+  /** Create a temporary file that contains configuration string specified by
+    * `--config-str`. This temporary file will be passed to `scalafmt-dynamic`.
     * See https://github.com/scalameta/scalafmt/pull/1367#issuecomment-464744077
     */
   private[this] val tempConfigPath: Option[Path] = configStr.map { s =>
@@ -119,12 +119,13 @@ case class CliOptions(
     file
   }
 
-  /** - If --config-str is specified (and tempConfigPath is defined),
-    *   this returns the path to a temporary file.
-    * - If both tempConfigPath and config are None,
-    *   this return the path to `.scalafmt.conf` on the working directory.
+  /**   - If --config-str is specified (and tempConfigPath is defined), this
+    *     returns the path to a temporary file.
+    *   - If both tempConfigPath and config are None, this return the path to
+    *     `.scalafmt.conf` on the working directory.
     *
-    * @return A path to a configuration file
+    * @return
+    *   A path to a configuration file
     */
   def configPath: Path =
     configPathOpt.get
@@ -138,10 +139,12 @@ case class CliOptions(
     .orElse(gitOps.rootDir.flatMap(tryGetConfigFile))
 
   /** Parse the scalafmt configuration and try to encode it to `ScalafmtConfig`.
-    * If `--config-str` is specified, this will parse the configuration string specified by `--config-str`.
-    * Otherwise, a contents of configuration file specified by `configPath` will be parsed.
+    * If `--config-str` is specified, this will parse the configuration string
+    * specified by `--config-str`. Otherwise, a contents of configuration file
+    * specified by `configPath` will be parsed.
     *
-    * If `--config-str` is not specified and configuration file is missing, this will return the default configuration
+    * If `--config-str` is not specified and configuration file is missing, this
+    * will return the default configuration
     */
   def scalafmtConfig: Configured[ScalafmtConfig] =
     hoconOpt.fold(Configured.ok(ScalafmtConfig.default))(Config.fromConf(_))
@@ -215,8 +218,7 @@ case class CliOptions(
       .flatMap(x => Try(Codec(x)).toOption)
       .getOrElse(ScalafmtConfig.default.encoding)
 
-  /** Returns None if .scalafmt.conf is not found or
-    * version setting is missing.
+  /** Returns None if .scalafmt.conf is not found or version setting is missing.
     */
   private[cli] def getVersionIfDifferent: Option[String] =
     getHoconValueOpt[String]("version").filter(_ != Versions.version)

scalafmt-cli/src/main/scala/org/scalafmt/cli/TermDisplay.scala
@@ -55,9 +55,8 @@ object Terminal {
 
     /** Clear the current line
       *
-      * n=0: clear from cursor to end of line
-      * n=1: clear from cursor to start of line
-      * n=2: clear entire line
+      * n=0: clear from cursor to end of line n=1: clear from cursor to start of
+      * line n=2: clear entire line
       */
     def clearLine(n: Int): Unit = control(n, 'K')
   }

scalafmt-cli/src/main/scala/org/scalafmt/cli/WriteMode.scala
@@ -2,8 +2,8 @@ package org.scalafmt.cli
 
 /** Determines the mode in which Scalafmt will behave
   *
-  * Override = Replace the file with its formatted form
-  * Stdout = Print the formatted file to Stdout (leaving the original file untouched)
+  * Override = Replace the file with its formatted form Stdout = Print the
+  * formatted file to Stdout (leaving the original file untouched)
   */
 sealed trait WriteMode
 

commit d40315cb54e564f47e618e2a69cac1543f963eb1

Fix scaladoc for elements broken in #2700

readme/src/main/scala/org/scalafmt/readme/Readme.scala
@@ -43,7 +43,11 @@ object Readme {
 
   /** repl session, inspired by tut.
     *
-    * Example: code="1 + 1" returns "scala> 1 + 1 res0: Int = 2"
+    * Example: {{{code="1 + 1"}}} returns
+    * {{{
+    *   scala> 1 + 1
+    *   res0: Int = 2
+    * }}}
     */
   def repl(code: String) = {
     import scala.meta._

scalafmt-cli/src/main/scala/org/scalafmt/cli/CliOptions.scala
@@ -17,8 +17,8 @@ object CliOptions {
   /** Tries to read configuration from
     *
     *   1. .scalafmt.conf in root dir of current git repo IF the following
-    *      setting is enabled: project.git = true 2. .scalafmt.conf from
-    *      init.common.workingDirectory
+    *      setting is enabled: project.git = true
+    *   1. .scalafmt.conf from init.common.workingDirectory
     *
     * I am happy to add alternative fallback methods for other VCS.
     *

scalafmt-cli/src/main/scala/org/scalafmt/cli/TermDisplay.scala
@@ -55,8 +55,9 @@ object Terminal {
 
     /** Clear the current line
       *
-      * n=0: clear from cursor to end of line n=1: clear from cursor to start of
-      * line n=2: clear entire line
+      *   - n=0: clear from cursor to end of line
+      *   - n=1: clear from cursor to start of line
+      *   - n=2: clear entire line
       */
     def clearLine(n: Int): Unit = control(n, 'K')
   }

scalafmt-cli/src/main/scala/org/scalafmt/cli/WriteMode.scala
@@ -2,8 +2,9 @@ package org.scalafmt.cli
 
 /** Determines the mode in which Scalafmt will behave
   *
-  * Override = Replace the file with its formatted form Stdout = Print the
-  * formatted file to Stdout (leaving the original file untouched)
+  *   - Override: Replace the file with its formatted form
+  *   - Stdout: Print the formatted file to Stdout (leaving the original file
+  *     untouched)
   */
 sealed trait WriteMode
 

scalafmt-core/shared/src/main/scala/org/scalafmt/config/Align.scala
@@ -7,33 +7,33 @@ import metaconfig.generic.Surface
 /** @param openParenCallSite
   *   If true AND bin-packing is true, then call-site arguments won't be aligned
   *   by the opening parenthesis. For example, this output will be disallowed
-  *
-  * function(a, b, c)
+  * {{{
+  *   function1(a,
+  *             b,
+  *             c)
+  * }}}
   * @param openParenDefnSite
   *   Same as [[openParenCallSite]], except definition site.
   * @param tokens
   *   The tokens to vertically align by. The "owner" is the
   *   scala.meta.Tree.getClass.getName of the deepest tree node that "owns" the
   *   token to align by.
+  *   - Examples:
+  * {{{
+  *   align.tokens = ["="] // align = owned by any tree node (not recommended)
   *
-  * Examples:
-  *
-  * align.tokens = ["="] // align = owned by any tree node (not recommended)
-  *
-  * align.tokens = [ { code = "=", owner = "Param" } // align = when owned by
-  * parameter tree nodes ]
-  *
-  * Pro tip. if you use for example
-  *
-  * preset = defaultWithAlign
-  *
-  * and want to add one extra token (for example "|>") to align by, write
-  *
-  * align.tokens."+" = [ "|> ]
-  *
-  * NOTE. Adding more alignment tokens may potentially decrease the vertical
-  * alignment in formatted output. Customize at your own risk, I recommend you
-  * try and stick to the default settings.
+  *   align.tokens = [
+  *     { code = "=", owner = "Param" } // align = when owned by parameter tree nodes
+  *   ]
+  * }}}
+  *   - Pro tip. if you use for example {{{preset = defaultWithAlign}}} and want
+  *     to add one extra token (for example "|>") to align by, write
+  * {{{
+  *     align.tokens."+" = [ "|> ]
+  * }}}
+  *   - NOTE. Adding more alignment tokens may potentially decrease the vertical
+  *     alignment in formatted output. Customize at your own risk, I recommend
+  *     you try and stick to the default settings.
   * @param arrowEnumeratorGenerator
   *   If true, aligns by `<-` in for comprehensions.
   * @param openParenCtrlSite
@@ -41,14 +41,20 @@ import metaconfig.generic.Surface
   *   indent at call site.
   * @param tokenCategory
   *   Customize which token kinds can align together. By default, only tokens
-  *   with the same `Token.productPrefix` align. To for example align = and <-,
-  *   set the values to: Map("Equals" -> "Assign", "LeftArrow" -> "Assign")
-  *   Note. Requires mixedTokens to be true.
+  *   with the same `Token.productPrefix` align. For example, to align = and <-,
+  *   set the values to:
+  * {{{
+  *     Map("Equals" -> "Assign", "LeftArrow" -> "Assign")
+  * }}}
+  * Note: Requires mixedTokens to be true.
   * @param treeCategory
   *   Customize which tree kinds can align together. By default, only trees with
-  *   the same `Tree.productPrefix` align. To for example align Defn.Val and
-  *   Defn.Var, set the values to: Map("Defn.Var" -> "Assign", "Defn.Val" ->
-  *   "Assign") Note. Requires mixedOwners to be true.
+  *   the same `Tree.productPrefix` align. For example, to align Defn.Val and
+  *   Defn.Var, set the values to:
+  * {{{
+  *     Map("Defn.Var" -> "Assign", "Defn.Val" -> "Assign")
+  * }}}
+  * Note. Requires mixedOwners to be true.
   * @param stripMargin
   *   If set, indent lines with a strip-margin character in a multiline string
   *   constant relative to the opening quotes (or the strip-margin character if

scalafmt-core/shared/src/main/scala/org/scalafmt/config/Newlines.scala
@@ -80,7 +80,13 @@ import metaconfig.generic.Surface
   * }}}
   * @param alwaysBeforeElseAfterCurlyIf
   *   if true, add a new line between the end of a curly if and the following
-  *   else. For example if(someCond) { // ... } else //...
+  *   else. For example
+  * {{{
+  *   if(someCond) {
+  *     // ...
+  *   }
+  *   else //...
+  * }}}
   * @param beforeMultilineDef
   *   If unfold (or true), add a newline before the body of a multiline def
   *   without curly braces. See #1126 for discussion. For example,
@@ -114,15 +120,16 @@ import metaconfig.generic.Surface
   *       }
   * }}}
   * @param source
-  *   Controls how line breaks in the input source are handled If `classic`
-  *   (default), the old mixed behaviour will be used If `keep`, try to keep
-  *   source newlines If `fold`, ignore source and try to remove line breaks If
-  *   `unfold`, ignore source and try to break lines
+  *   Controls how line breaks in the input source are handled
+  *   - If `classic` (default), the old mixed behaviour will be used
+  *   - If `keep`, try to keep source newlines
+  *   - If `fold`, ignore source and try to remove line breaks
+  *   - If `unfold`, ignore source and try to break lines
   * @param afterInfix
-  *   Controls how line breaks around operations are handled If `keep` (default
-  *   for source=classic,keep), preserve existing If `some` (default for
-  *   source=fold), break after some infix ops If `many` (default for
-  *   source=unfold), break after many infix ops
+  *   Controls how line breaks around operations are handled
+  *   - If `keep` (default for source=classic,keep), preserve existing
+  *   - If `some` (default for source=fold), break after some infix ops
+  *   - If `many` (default for source=unfold), break after many infix ops
   * @param afterInfixBreakOnNested
   *   Force breaks around nested (enclosed in parentheses) expressions
   * @param afterInfixMaxCountPerFile

scalafmt-core/shared/src/main/scala/org/scalafmt/config/OptIn.scala
@@ -118,11 +118,12 @@ case class OptIn(
   /** See https://github.com/scalameta/scalafmt/issues/1712
     *
     * Setting behavior and name were mirrored. After deprecation and right
-    * naming we need to: if `forceBlankLineBeforeDocstring` (new name) has
-    * default value (true) fallback to `blankLineBeforeDocstring` (old config)
-    * which may be configured in .scalafmt.conf if
-    * `forceBlankLineBeforeDocstring` configured to non-default value don't look
-    * at the old name
+    * naming we need to:
+    *   - if `forceBlankLineBeforeDocstring` (new name) has default value
+    *     (true), fallback to `blankLineBeforeDocstring` (old config) which may
+    *     be configured in .scalafmt.conf
+    *   - if `forceBlankLineBeforeDocstring` configured to non-default value,
+    *     don't look at the old name
     */
   lazy val forceNewlineBeforeDocstringSummary: Boolean =
     forceBlankLineBeforeDocstring && !blankLineBeforeDocstring

scalafmt-core/shared/src/main/scala/org/scalafmt/config/ScalafmtConfig.scala
@@ -30,32 +30,48 @@ import org.scalafmt.util.ValidationOps
   * @param danglingParentheses
   *   If true AND @binPackArguments is true AND @configStyleArguments is false,
   *   then this
-  *
-  * function( longerArg1, longerArg3)
-  *
+  * {{{
+  *     function(
+  *         longerArg1,
+  *         longerArg3)
+  * }}}
   * is formatted like this
-  *
-  * function( longerArg1, longerArg3 )
+  * {{{
+  *     function(
+  *         longerArg1,
+  *         longerArg3
+  *     )
+  * }}}
   * @param rewriteTokens
   *   Map of tokens to rewrite. For example, Map("⇒" -> "=>") will rewrite
   *   unicode arrows to regular ascii arrows.
   * @param importSelectors
   *   Controls formatting of import selectors with multiple names from the same
-  *   package; If [[org.scalafmt.config.ImportSelectors.binPack]], import
-  *   selectors are arranged to fit within the maximum line width If
-  *   [[org.scalafmt.config.ImportSelectors.noBinPack]], import selectors are
-  *   broken to one per line If
-  *   [[org.scalafmt.config.ImportSelectors.singleLine]], import selectors are
-  *   kept on a single line The default setting is currently `noBinPack`.
+  *   package
+  *   - If [[org.scalafmt.config.ImportSelectors.binPack]], import selectors are
+  *     arranged to fit within the maximum line width
+  *   - If [[org.scalafmt.config.ImportSelectors.noBinPack]], import selectors
+  *     are broken to one per line
+  *   - If [[org.scalafmt.config.ImportSelectors.singleLine]], import selectors
+  *     are kept on a single line The default setting is currently `noBinPack`.
   * @param indentYieldKeyword
-  *   If true, indents `yield` by two spaces for (i <- j) yield banana If false,
-  *   treats `yield` like `else` for (i <- j) yield banana
+  *   - If true, indents `yield` by two spaces
+  * {{{
+  *       for (i <- j)
+  *         yield banana
+  * }}}
+  *   - If false, treats `yield` like `else`
+  * {{{
+  *       for (i <- j)
+  *       yield banana
+  * }}}
   * @param lineEndings
-  *   If [[LineEndings.unix]], output will include only unix line endings If
-  *   [[LineEndings.windows]], output will include only windows line endings If
-  *   [[LineEndings.preserve]], output will include endings included in original
-  *   file (windows if there was at least one windows line ending, unix if there
-  *   was zero occurrences of windows line endings)
+  *   - If [[LineEndings.unix]], output will include only unix line endings
+  *   - If [[LineEndings.windows]], output will include only windows line
+  *     endings
+  *   - If [[LineEndings.preserve]], output will include endings included in
+  *     original file (windows if there was at least one windows line ending,
+  *     unix if there was zero occurrences of windows line endings)
   * @param includeCurlyBraceInSelectChains
   *   NB: failure unless newlines.source=classic If true, includes curly brace
   *   applications in select chains/pipelines.

scalafmt-core/shared/src/main/scala/org/scalafmt/config/ScalafmtOptimizer.scala
@@ -24,17 +24,23 @@ import metaconfig._
   *   Do not optimize inside certain areas such as term apply.
   * @param pruneSlowStates
   *   Eliminate solutions that move slower than other solutions.
+  *   - If a solution reaches a point X first and other solution that reaches
+  *     the same point later, the first solution is preferred if it can be
+  *     verified to be always better (see
+  *     [[org.scalafmt.internal.State.alwaysBetter]]).
+  *   - Note. This affects the output positively because it breaks a tie between
+  *     two equally expensive solutions by eliminating the slower one.
   *
-  * If a solution reaches a point X first and other solution that reaches the
-  * same point later, the first solution is preferred if it can be verified to
-  * be always better (see [[org.scalafmt.internal.State.alwaysBetter]]).
-  *
-  * Note. This affects the output positively because it breaks a tie between two
-  * equally expensive solutions by eliminating the slower one.
-  *
-  * Example, solution 1 is preferred even though both solutions cost the same:
-  *
-  * // solution 1 a + b + c + d // solution 2 a + b + c + d
+  *   - Example: solution 1 is preferred even though both solutions cost the
+  *     same:
+  * {{{
+  *       // solution 1
+  *       a + b +
+  *       c + d
+  *       // solution 2
+  *       a +
+  *       b + c + d
+  * }}}
   * @param recurseOnBlocks
   *   Recursively format { ... } blocks inside no optimization zones.
   *

scalafmt-core/shared/src/main/scala/org/scalafmt/config/Spaces.scala
@@ -9,24 +9,40 @@ import metaconfig._
   * @param afterTripleEquals
   *   If true, formats ===( as === (
   * @param inImportCurlyBraces
-  *   If true, formats `import a.b.{ c, d }`. If false, formats `import a.b.{c,
-  *   d}`.
+  *   - If true, formats
+  * {{{
+  *     import a.b.{ c, d}
+  * }}}
+  *   - If false, formats
+  * {{{
+  *     import a.b.{c, d}
+  * }}}
   * @param inInterpolatedStringCurlyBraces
-  *   If true, formats `s"\${var1} \${var2.sub}"` as `s"\${ var1 } \${ var2.sub
-  *   }"`.
+  *   If true, formats
+  * {{{
+  *     s"\${var1} \${var2.sub}"
+  * }}}
+  * as
+  * {{{
+  *     s"\${ var1 } \${ var2.sub }"
+  * }}}
   * @param inParentheses
   *   If true, formats `foo(a, b)` as `foo( a, b )`.
   * @param neverAroundInfixTypes
-  *   If ["##"] is specified as operator then formats `Generic[Foo] ## Repr` as
-  *   `Generic[Foo]##Repr`.
+  *   If ["##"] is specified as operator then formats {{{Generic[Foo] ## Repr}}}
+  *   as {{{Generic[Foo]##Repr}}}.
   * @param afterKeywordBeforeParen
   *   if false, does not add a space between a keyword and a parenthesis. For
-  *   example: if(a) println("HELLO!") while(a) println("HELLO!")
+  *   example:
+  * {{{
+  *     if(a) println("HELLO!")
+  *     while(a) println("HELLO!")
+  * }}}
   * @param inByNameTypes
-  *   If false, removes space in by-name parameter. `def foo(a: =>A)`
+  *   If false, removes space in by-name parameter: {{{def foo(a: =>A)}}}
   * @param afterSymbolicDefs
-  *   If true, adds a single space after an operator method For example: def <=>
-  *   [T](that: T): Boolean
+  *   If true, adds a single space after an operator method. For example:
+  *   {{{def <=> [T](that: T): Boolean}}}
   */
 case class Spaces(
     beforeContextBoundColon: Spaces.BeforeContextBound =

scalafmt-core/shared/src/main/scala/org/scalafmt/internal/Indent.scala
@@ -36,8 +36,10 @@ object Length {
   /** Indent up to the column of the left token.
     *
     * Example: the opening parenthesis below indents by [[StateColumn]].
-    *
-    * foobar(arg1, arg2)
+    * {{{
+    *   foobar1(arg1,
+    *           arg2)
+    * }}}
     */
   case object StateColumn extends Length {
     override def withStateOffset(offset: Int): Int = offset

scalafmt-core/shared/src/main/scala/org/scalafmt/rewrite/Imports.scala
@@ -564,9 +564,18 @@ object Imports extends RewriteFactory {
     }
   }
 
-  /** convert import a.{c, b}, d.{f, e} to import a.b import a.c
+  /** convert
+    * {{{
+    *   import a.{c, b}, d.{f, e}
+    * }}}
+    * to
+    * {{{
+    *   import a.b
+    *   import a.c
     *
-    * import d.e import d.f
+    *   import d.e
+    *   import d.f
+    * }}}
     */
   private class ExpandFull(implicit ctx: RewriteCtx) extends ExpandBase {
     override protected final def addClauseToGroup(
@@ -589,9 +598,16 @@ object Imports extends RewriteFactory {
     }
   }
 
-  /** convert import a.{c, b}, d.{f, e} to import a.{b, c}
+  /** convert
+    * {{{
+    *   import a.{c, b}, d.{f, e}
+    * }}}
+    * to
+    * {{{
+    *   import a.{b, c}
     *
-    * import d.{e, f}
+    *   import d.{e, f}
+    * }}}
     */
   private class ExpandPart(implicit ctx: RewriteCtx) extends ExpandBase {
     override protected final def addClauseToGroup(
@@ -603,7 +619,14 @@ object Imports extends RewriteFactory {
       addToGroup(group, kw, ref, importer.importees, importer)
   }
 
-  /** convert import a.{c, b}, d.{f, e} to import a.{b, c}, d.{e, f}
+  /** convert
+    * {{{
+    *   import a.{c, b}, d.{f, e}
+    * }}}
+    * to
+    * {{{
+    *   import a.{b, c}, d.{e, f}
+    * }}}
     */
   private class ExpandNone(implicit ctx: RewriteCtx) extends Base {
     override protected def processImports(

scalafmt-core/shared/src/main/scala/org/scalafmt/rewrite/PreferCurlyFors.scala
@@ -38,12 +38,17 @@ object PreferCurlyFors extends Rewrite with FormatTokensRewrite.RuleFactory {
   * with braces and new-lines.
   *
   * For example,
-  *
-  * for(a <- as; b <- bs if b > 2) yield (a, b)
-  *
-  * becomes,
-  *
-  * for { a <- as b <- bs if b > 2 } yield (a, b)
+  * {{{
+  *   for(a <- as; b <- bs if b > 2) yield (a, b)
+  * }}}
+  * becomes
+  * {{{
+  *   for {
+  *     a <- as
+  *     b <- bs
+  *     if b > 2
+  *   } yield (a, b)
+  * }}}
   */
 private class PreferCurlyFors(ftoks: FormatTokens)
     extends FormatTokensRewrite.Rule {

scalafmt-core/shared/src/main/scala/org/scalafmt/rewrite/RedundantBraces.scala
@@ -189,10 +189,11 @@ class RedundantBraces(ftoks: FormatTokens) extends FormatTokensRewrite.Rule {
       syntax.headOption.contains('`') && syntax.lastOption.contains('`')
     }
 
-    /** we need remain braces for interpolated literal identifiers: s"string
-      * ${`type`}" and identifiers started with '_': s"string %{_id}" otherwise
-      * formatting will result in compilation error (see
-      * https://github.com/scalameta/scalafmt/issues/1420)
+    /** we need to keep braces
+      *   - for interpolated literal identifiers: {{{s"string ${`type`}"}}}
+      *   - and identifiers starting with '_': {{{s"string %{_id}"}}}, otherwise
+      *     formatting will result in compilation error (see
+      *     https://github.com/scalameta/scalafmt/issues/1420)
       */
     def shouldTermBeEscaped(arg: Term.Name): Boolean =
       arg.value.head == '_' || isLiteralIdentifier(arg)

scalafmt-core/shared/src/main/scala/org/scalafmt/util/LiteralOps.scala
@@ -8,10 +8,15 @@ object LiteralOps {
     *
     * Divides literal into three parts:
     *
-    * 0xFF123L 0x is a hex prefix FF123 is a body L is a long suffix
+    * for 0xFF123L
+    *   - 0x is a hex prefix
+    *   - FF123 is a body
+    *   - L is a long suffix
     *
-    * literals.hexPrefix applies prefix, literals.hexDigits applies to body and
-    * literals.long applies to suffix
+    * Then
+    *   - literals.hexPrefix applies to prefix
+    *   - literals.hexDigits applies to body and
+    *   - literals.long applies to suffix
     */
   def prettyPrintInteger(
       str: String
@@ -33,11 +38,13 @@ object LiteralOps {
     *
     * Divides literals into two parts:
     *
-    * 1.0e-10f
-    * 1.0e-10 is a body with scientific notation f is a float/double suffix
+    * For 1.0e-10f
+    *   - 1.0e-10 is a body with scientific notation
+    *   - f is a float/double suffix
     *
-    * literals.scientific applies to body and literals.float/double applies to
-    * suffix
+    * Then
+    *   - literals.scientific applies to body and
+    *   - literals.float/double applies to suffix
     */
   private def prettyPrintFloatingPoint(
       str: String,

scalafmt-core/shared/src/main/scala/org/scalafmt/util/TokenOps.scala
@@ -25,10 +25,11 @@ object TokenOps {
     * from different source files. We only care about getting a unique
     * identifier for the token inside this source file.
     *
-    * The hash code works like this this: Top 8 bits go to a hashCode of
-    * productPrefix, a unique identifier for the tokens class. Next 28 bits go
-    * to the tokens **start** offset byte. Final 28 bits go to the tokens
-    * **end** offset byte.
+    * The hash code works like this this:
+    *   - Top 8 bits go to a hashCode of productPrefix, a unique identifier for
+    *     the tokens class
+    *   - Next 28 bits go to the tokens **start** offset byte
+    *   - Final 28 bits go to the tokens **end** offset byte.
     *
     * The only chance for collision is if two empty length tokens with the same
     * type lie next to each other. @xeno-by said this should not happen.

scalafmt-tests/src/test/scala/org/scalafmt/cli/FileTestOps.scala
@@ -24,9 +24,14 @@ object FileTestOps {
   }
 
   /** Gives a string representation of a directory. For example
-    *
-    * /build.sbt val x = project /src/main/scala/Main.scala object A { def main
-    * = Unit } /target/scala-2.11/foo.class ^!*@#@!*#&@*!&#^
+    * {{{
+    * /build.sbt
+    * val x = project
+    * /src/main/scala/Main.scala
+    * object A { def main = Unit }
+    * /target/scala-2.11/foo.class
+    * ^!*@#@!*#&@*!&#^
+    * }}}
     */
   def dir2string(file: AbsoluteFile): String = {
     val rootPath = file.path