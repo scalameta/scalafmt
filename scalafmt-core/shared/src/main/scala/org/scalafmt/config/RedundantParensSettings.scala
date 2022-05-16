6691bc017461a80f91cf7467243f0153ecbf1f0c
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2022-05-15 22:32:29 -0700

    RedundantParens: define infixSide parameter

--- scalafmt-core/shared/src/main/scala/org/scalafmt/config/RedundantParensSettings.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/config/RedundantParensSettings.scala
@@ -3,6 +3,7 @@ package org.scalafmt.config
 import metaconfig._
 
 case class RedundantParensSettings(
+    infixSide: Option[RedundantParensSettings.InfixSide] = None
 )
 
 object RedundantParensSettings {
@@ -11,4 +12,14 @@ object RedundantParensSettings {
     generic.deriveSurface
   implicit lazy val codec: ConfCodecEx[RedundantParensSettings] =
     generic.deriveCodecEx(default).noTypos
+
+  sealed abstract class InfixSide
+  object InfixSide {
+    implicit val codec: ConfCodecEx[InfixSide] =
+      ReaderUtil.oneOf[InfixSide](all, many, some)
+    case object all extends InfixSide
+    case object many extends InfixSide
+    case object some extends InfixSide
+  }
+
 }
