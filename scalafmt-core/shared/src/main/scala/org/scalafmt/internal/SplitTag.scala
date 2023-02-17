2fbe2f0381a0206560ab2dd0f7661c94cd8f2548
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2023-02-16 16:12:24 -0800

    FormatOps vertical multiline: fix implicit prefer
    The case of implicitParamListModifierPrefer=after setting (default) was
    incorrectly handled with vertical multiline. The test changes show that
    it wasn't working for cases with multiple parameters in a group.

--- scalafmt-core/shared/src/main/scala/org/scalafmt/internal/SplitTag.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/internal/SplitTag.scala
@@ -14,5 +14,6 @@ object SplitTag {
   case object SelectChainSecondNL extends SplitTag
   case object InfixChainNoNL extends SplitTag
   case object OnelineWithChain extends SplitTag
+  case object VerticalMultilineSingleLine extends SplitTag
 
 }
