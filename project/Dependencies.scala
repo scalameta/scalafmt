e0a27b5927a3d0dd8d836b666dbc7c713dc26eb1
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-04-01 07:54:43 -0700

    scalafmt: enable AvoidInfix rewrite

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,8 @@ object Dependencies {
 
   val scalacheck = "org.scalacheck" %% "scalacheck" % scalacheckV
   val munit      = Def.setting("org.scalameta" %%% "munit" % munitV)
-  val scalameta  = Def.setting("org.scalameta" %%% "scalameta" % scalametaV excludeAll scalapb.value)
+  val scalameta = Def
+    .setting(("org.scalameta" %%% "scalameta" % scalametaV).excludeAll(scalapb.value))
 
   val metaconfig         = Def.setting("com.geirsson" %%% "metaconfig-core" % metaconfigV)
   val metaconfigTypesafe = Def.setting("com.geirsson" %%% "metaconfig-typesafe-config" % metaconfigV)
