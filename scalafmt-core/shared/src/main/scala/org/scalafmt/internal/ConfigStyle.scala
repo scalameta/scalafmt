d8d4842f7740d977b93e9fe7b02b723cfebf44d9
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-05-12 11:11:41 -0700

    Router: Align binpack.{call,defn}Site behaviour
    Also, modify it so that it matches configuration parameters better. In
    most cases, it will either preserve formatting (which tests may not show
    as they operate on non-formatted code) or can easily be configured to
    match existing formatting.
    
    This change partially restores some of formatting changes introduced in
    recent commits, by making their results more consistent with new logic.

--- scalafmt-core/shared/src/main/scala/org/scalafmt/internal/ConfigStyle.scala
+++
@@ -1,9 +0,0 @@
-package org.scalafmt.internal
-
-sealed abstract class ConfigStyle
-
-object ConfigStyle {
-  case object None extends ConfigStyle
-  case object Source extends ConfigStyle
-  case object Forced extends ConfigStyle
-}
