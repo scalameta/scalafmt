a21752471e355f490e1416d8f97cc18285113bca
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-06-14 07:06:13 -0700

    Scalameta: upgrade to v4.9.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 
 object Dependencies {
   val metaconfigV = "0.12.0"
-  val scalametaV  = "4.9.5"
+  val scalametaV  = "4.9.6"
   val scalacheckV = "1.18.0"
   val coursier    = "2.1.2"
   val munitV      = "1.0.0"
