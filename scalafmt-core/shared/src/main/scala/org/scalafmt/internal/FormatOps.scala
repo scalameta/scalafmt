2446ba60c5ca4c1d6f28939fa027f9b0ce9376b7
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-08-31 17:52:37 -0700

    FormatOps: opt-braces `catch` uses newlines.source

--- scalafmt-core/shared/src/main/scala/org/scalafmt/internal/FormatOps.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/internal/FormatOps.scala
@@ -2161,7 +2161,8 @@ class FormatOps(
           val x = t.catchp
           val nlOnly = x match {
             // to avoid next expression being interpreted as body
-            case head :: Nil => isEmptyTree(head.body) && t.finallyp.isEmpty
+            case head :: Nil => shouldBreakInOptionalBraces(ft, nft) ||
+              t.finallyp.isEmpty && isEmptyTree(head.body)
             case _ => true
           }
           WithStats(ft, nft, x.headOption, x.last, Some(t), nlOnly = nlOnly)
