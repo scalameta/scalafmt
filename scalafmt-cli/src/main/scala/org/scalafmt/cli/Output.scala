1b1d57d2dcd37a6858d77f26265f5f483e747afb
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-09-15 15:45:44 -0700

    CliOptions: write info to console instead of err
    Currently, info is written to out if out is not used for result output,
    and to err otherwise.
    
    Let's enhance this approach as follows: if out is not used, then we'll
    write into to out, as before; otherwise, before resorting to err, check
    if the console is available and use it then.

--- scalafmt-cli/src/main/scala/org/scalafmt/cli/Output.scala
+++ scalafmt-cli/src/main/scala/org/scalafmt/cli/Output.scala
@@ -1,6 +1,7 @@
 package org.scalafmt.cli
 
 import java.io._
+import java.nio.charset._
 
 object Output {
 
@@ -27,6 +28,25 @@ object Output {
     override def outputStream: OutputStream = obj
     override def printStream: PrintStream = obj
     override def printWriter: PrintWriter = new PrintWriter(obj)
+  }
+
+  class FromWriter(val obj: Writer, charset: Charset = StandardCharsets.UTF_8)
+      extends OutputStream with StreamOrWriter {
+
+    override def write(b: Int): Unit = obj.write(b & 0xf)
+    override def write(b: Array[Byte]): Unit = obj.write(new String(b, charset))
+    override def write(b: Array[Byte], off: Int, len: Int): Unit = obj
+      .write(new String(b, off, len, charset))
+
+    override def flush(): Unit = obj.flush()
+    override def close(): Unit = obj.close()
+
+    def outputStream: OutputStream = this
+    override def printStream: PrintStream = new PrintStream(this)
+    override def printWriter: PrintWriter = obj match {
+      case x: PrintWriter => x
+      case _ => new PrintWriter(obj)
+    }
   }
 
 }
