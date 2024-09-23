5c882935bd75b89cf674cf4ad06ea87e37f6a067
Jan Chyb <jan.chyb.99@gmail.com>
2024-09-23 18:12:03 +0200

    Rewrite interfaces to scala for Scala Native

---
+++ scalafmt-interfaces/native/src/main/java/org/scalafmt/interfaces/PositionException.scala
@@ -0,0 +1,37 @@
+package org.scalafmt.interfaces
+
+import java.nio.file.Path
+
+/** An exception that happened at a position in a source file such as a parse
+  * error.
+  */
+abstract class PositionException(message: String, cause: Throwable)
+    extends Exception(message, cause) {
+
+  override def fillInStackTrace(): Throwable = synchronized(this)
+
+  /** @return
+    *   The file where the error occurred.
+    */
+  def file(): Path
+
+  /** @return
+    *   The text contents of the file being formatted.
+    */
+  def code(): String
+
+  /** @return
+    *   The fully formatted error message including line content and caret.
+    */
+  def longMessage(): String
+
+  /** @return
+    *   Only the error message itself without line content and caret.
+    */
+  def shortMessage(): String
+
+  def startLine(): Int
+  def startCharacter(): Int
+  def endLine(): Int
+  def endCharacter(): Int
+}
