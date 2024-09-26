6e624bd123953847fbfd23868eedd108a1d87cc7
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-09-26 14:41:05 -0700

    CommunityRepoSuite: add common abstract superclass

--- scalafmt-tests-community/src/test/scala/org/scalafmt/community/CommunityScala3Suite.scala
+++ scalafmt-tests-community/src/test/scala/org/scalafmt/community/CommunityScala3Suite.scala
@@ -2,48 +2,29 @@ package org.scalafmt.community
 
 import scala.meta._
 
-abstract class CommunityScala3Suite extends CommunitySuite {
+abstract class CommunityScala3Suite(name: String)
+    extends CommunityRepoSuite("https://github.com/scala/scala3.git", name)
 
-  protected def getBuild(
-      name: String,
-      ref: String,
-      dialect: sourcecode.Text[Dialect],
-      files: Int,
-  ) = CommunityBuild(
-    "https://github.com/scala/scala3.git",
-    ref,
-    name,
-    Nil,
-    files,
-    dialect,
-  )
+class CommunityScala3_0Suite extends CommunityScala3Suite("scala-3.0") {
+
+  override protected def builds = Seq(getBuild("3.0.2", dialects.Scala30, 698))
 
 }
 
-class CommunityScala3_0Suite extends CommunityScala3Suite {
+class CommunityScala3_1Suite extends CommunityScala3Suite("scala-3.1") {
 
-  override protected def builds =
-    Seq(getBuild("scala-3.0", "3.0.2", dialects.Scala30, 698))
-
-}
-
-class CommunityScala3_1Suite extends CommunityScala3Suite {
-
-  override protected def builds =
-    Seq(getBuild("scala-3.1", "3.1.3", dialects.Scala31, 747))
+  override protected def builds = Seq(getBuild("3.1.3", dialects.Scala31, 747))
 
 }
 
-class CommunityScala3_2Suite extends CommunityScala3Suite {
+class CommunityScala3_2Suite extends CommunityScala3Suite("scala-3.2") {
 
-  override protected def builds =
-    Seq(getBuild("scala-3.2", "3.2.2", dialects.Scala32, 791))
+  override protected def builds = Seq(getBuild("3.2.2", dialects.Scala32, 791))
 
 }
 
-class CommunityScala3_3Suite extends CommunityScala3Suite {
+class CommunityScala3_3Suite extends CommunityScala3Suite("scala-3.3") {
 
-  override protected def builds =
-    Seq(getBuild("scala-3.3", "3.3.3", dialects.Scala33, 861))
+  override protected def builds = Seq(getBuild("3.3.3", dialects.Scala33, 861))
 
 }
