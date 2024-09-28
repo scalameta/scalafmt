b5d1f9e91d1d60afef20aa34aa6f029f2917bd4b
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-09-28 09:22:22 -0700

    CommunitySparkSuite: add spark v3.5.3

--- scalafmt-tests-community/spark/src/test/scala/org/scalafmt/community/spark/CommunitySparkSuite.scala
+++ scalafmt-tests-community/spark/src/test/scala/org/scalafmt/community/spark/CommunitySparkSuite.scala
@@ -27,3 +27,10 @@ class CommunitySpark3_4Suite extends CommunitySparkSuite("spark-3.4") {
     Seq(getBuild("v3.4.1", dialects.Scala213, 2585))
 
 }
+
+class CommunitySpark3_5Suite extends CommunitySparkSuite("spark-3.5") {
+
+  override protected def builds =
+    Seq(getBuild("v3.5.3", dialects.Scala213, 2756))
+
+}
