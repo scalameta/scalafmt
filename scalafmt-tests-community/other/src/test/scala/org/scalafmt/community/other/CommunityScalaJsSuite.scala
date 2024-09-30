943487ec9487c2efe70e2557c378538dad736829
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-09-29 22:38:04 -0700

    CommunitySuite: test scala.js version 1.17.0

---
+++ scalafmt-tests-community/other/src/test/scala/org/scalafmt/community/other/CommunityScalaJsSuite.scala
@@ -0,0 +1,15 @@
+package org.scalafmt.community.other
+
+import org.scalafmt.community.common.CommunityRepoSuite
+
+import scala.meta._
+
+abstract class CommunityScalaJsSuite(name: String)
+    extends CommunityRepoSuite("https://github.com/scala-js/scala-js.git", name)
+
+class CommunityScalaJs1_17Suite extends CommunityScalaJsSuite("scala-js-1.17") {
+
+  override protected def builds =
+    Seq(getBuild("v1.17.0", dialects.Scala213, 787))
+
+}
