5512fa6407b229241e16933a4a09e7103ce56261
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-10-21 07:18:13 -0700

    Add preset values to overflow, redundant delims

--- scalafmt-core/shared/src/main/scala/org/scalafmt/config/RedundantParensSettings.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/config/RedundantParensSettings.scala
@@ -11,10 +11,18 @@ object RedundantParensSettings {
   private[scalafmt] val all =
     RedundantParensSettings(infixSide = Some(InfixSide.all))
 
+  private implicit val preset
+      : PartialFunction[Conf, RedundantParensSettings] = {
+    case Conf.Str("default") => default
+    case Conf.Str("all") => all
+  }
+
   implicit lazy val surface: generic.Surface[RedundantParensSettings] =
     generic.deriveSurface
-  implicit lazy val codec: ConfCodecEx[RedundantParensSettings] = generic
-    .deriveCodecEx(default).noTypos
+  implicit lazy val encoder: ConfEncoder[RedundantParensSettings] =
+    generic.deriveEncoder
+  implicit lazy val decoder: ConfDecoderEx[RedundantParensSettings] = Presets
+    .mapDecoder(generic.deriveDecoderEx(default).noTypos, "RedundantParens")
 
   sealed abstract class InfixSide
   object InfixSide {
