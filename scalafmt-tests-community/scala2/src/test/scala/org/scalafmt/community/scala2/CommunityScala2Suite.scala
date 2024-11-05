0b7c2b0a52a8f865ca16deec80be4cbf2766d5a3
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-11-05 12:31:39 -0800

    Router: lower NL penalty for braces-to-parens case
    This will give slight preference NOT to rewrite and avoid non-idempotent
    formatting.

--- scalafmt-tests-community/scala2/src/test/scala/org/scalafmt/community/scala2/CommunityScala2Suite.scala
+++ scalafmt-tests-community/scala2/src/test/scala/org/scalafmt/community/scala2/CommunityScala2Suite.scala
@@ -9,7 +9,7 @@ abstract class CommunityScala2Suite(name: String)
 
 class CommunityScala2_12Suite extends CommunityScala2Suite("scala-2.12") {
 
-  override protected def totalStatesVisited: Option[Int] = Some(34661174)
+  override protected def totalStatesVisited: Option[Int] = Some(34661248)
 
   override protected def builds =
     Seq(getBuild("v2.12.20", dialects.Scala212, 1277))
@@ -18,7 +18,7 @@ class CommunityScala2_12Suite extends CommunityScala2Suite("scala-2.12") {
 
 class CommunityScala2_13Suite extends CommunityScala2Suite("scala-2.13") {
 
-  override protected def totalStatesVisited: Option[Int] = Some(43184755)
+  override protected def totalStatesVisited: Option[Int] = Some(43184810)
 
   override protected def builds =
     Seq(getBuild("v2.13.14", dialects.Scala213, 1287))
