447c3425d7c327c01a56d53b4419bd5870bb414a
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-11-20 08:30:57 -0800

    DynamicSuite: test Scalafmt interface class loader

---
+++ scalafmt-dynamic/native/src/test/scala/org/scalafmt/dynamic/DynamicSuite.scala
@@ -0,0 +1,15 @@
+package org.scalafmt.dynamic
+
+import org.scalafmt.interfaces._
+
+import munit.FunSuite
+
+class DynamicSuite extends FunSuite {
+
+  test("Scalafmt interface") {
+    interceptMessage[ScalafmtException] {
+      "Can't use different version for native CLI"
+    }(Scalafmt.create(this.getClass.getClassLoader))
+  }
+
+}
