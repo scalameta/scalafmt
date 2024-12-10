97f3f3a96c7094b4a001a1ac49f3aa092516281c
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-12-09 17:30:13 -0800

    Router: for/infix before select like fewer-braces

--- scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
+++ scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
@@ -144,7 +144,7 @@ class FormatTests extends FunSuite with CanRunTests with FormatAssertions {
     val explored = Debug.explored.get()
     logger.debug(s"Total explored: $explored")
     if (!onlyUnit && !onlyManual)
-      assertEquals(explored, 1192570, "total explored")
+      assertEquals(explored, 1193368, "total explored")
     val results = debugResults.result()
     // TODO(olafur) don't block printing out test results.
     // I don't want to deal with scalaz's Tasks :'(
