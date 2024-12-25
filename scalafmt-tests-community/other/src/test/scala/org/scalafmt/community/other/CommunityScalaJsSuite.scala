239ae71958fded66aa876914d52d12b8ad898780
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-12-25 08:01:58 -0800

    CommunityTest: re-enable fold-rewrites in scalajs

--- scalafmt-tests-community/other/src/test/scala/org/scalafmt/community/other/CommunityScalaJsSuite.scala
+++ scalafmt-tests-community/other/src/test/scala/org/scalafmt/community/other/CommunityScalaJsSuite.scala
@@ -1,7 +1,6 @@
 package org.scalafmt.community.other
 
 import org.scalafmt.community.common.CommunityRepoSuite
-import org.scalafmt.community.common.TestStyles
 
 import scala.meta._
 
@@ -10,12 +9,6 @@ abstract class CommunityScalaJsSuite(name: String)
 
 class CommunityScalaJs1_17Suite extends CommunityScalaJsSuite("scala-js-1.17") {
 
-  override protected def builds = Seq(getBuild(
-    "v1.17.0",
-    dialects.Scala213,
-    787,
-    styles = Seq(TestStyles.foldWithRewritesAndOverflow),
-    stylesIncluded = false,
-  ))
+  override protected def builds = Seq(getBuild("v1.17.0", dialects.Scala213, 787))
 
 }
