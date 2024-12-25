8cac48cf04892952bc289b5ea50f3f74b7a93555
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-12-25 12:14:30 -0800

    RedundantBraces: improve check around infix
    It appears that the check around opening brace is not important but the
    check around closing brace should take into account whether there's a
    trailing comma or if the infix continues after the right brace.

--- scalafmt-tests-community/intellij/src/test/scala/org/scalafmt/community/intellij/CommunityIntellijScalaSuite.scala
+++ scalafmt-tests-community/intellij/src/test/scala/org/scalafmt/community/intellij/CommunityIntellijScalaSuite.scala
@@ -13,7 +13,7 @@ abstract class CommunityIntellijScalaSuite(name: String)
 class CommunityIntellijScala_2024_2_Suite
     extends CommunityIntellijScalaSuite("intellij-scala-2024.2") {
 
-  override protected def totalStatesVisited: Option[Int] = Some(59238466)
+  override protected def totalStatesVisited: Option[Int] = Some(59238459)
 
   override protected def builds = Seq(getBuild(
     "2024.2.28",
@@ -52,7 +52,7 @@ class CommunityIntellijScala_2024_2_Suite
 class CommunityIntellijScala_2024_3_Suite
     extends CommunityIntellijScalaSuite("intellij-scala-2024.3") {
 
-  override protected def totalStatesVisited: Option[Int] = Some(59452507)
+  override protected def totalStatesVisited: Option[Int] = Some(59452500)
 
   override protected def builds = Seq(getBuild(
     "2024.3.4",
