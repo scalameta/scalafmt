dbf2fe1d1f7245e9f9f09a9e16506cd27d57ac6b
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-12-31 07:24:45 -0800

    Test comments around implicit in param clause

--- scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
+++ scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
@@ -144,7 +144,7 @@ class FormatTests extends FunSuite with CanRunTests with FormatAssertions {
     val explored = Debug.explored.get()
     logger.debug(s"Total explored: $explored")
     if (!onlyUnit && !onlyManual)
-      assertEquals(explored, 1244442, "total explored")
+      assertEquals(explored, 1245376, "total explored")
     val results = debugResults.result()
     // TODO(olafur) don't block printing out test results.
     // I don't want to deal with scalaz's Tasks :'(
