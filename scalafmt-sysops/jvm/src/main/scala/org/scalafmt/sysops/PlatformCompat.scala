bb173d52a6bac3ee1ab13bdf08e5a91f3db3f59a
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-02-02 07:45:19 -0800

    PlatformCompat: inline `fixPathOnNativeWindows`

--- scalafmt-sysops/jvm/src/main/scala/org/scalafmt/sysops/PlatformCompat.scala
+++ scalafmt-sysops/jvm/src/main/scala/org/scalafmt/sysops/PlatformCompat.scala
@@ -5,7 +5,6 @@ import scala.concurrent.ExecutionContext
 private[scalafmt] object PlatformCompat {
   def isScalaNative = false
   def prepareCommand(cmd: Seq[String]) = cmd
-  def fixPathOnNativeWindows(path: String) = path
   def isNativeOnWindows = false
   def relativize(base: AbsoluteFile, path: AbsoluteFile) = base.toUri
     .relativize(path.toUri)
--- scalafmt-sysops/native/src/main/scala/org/scalafmt/sysops/PlatformCompat.scala
+++ scalafmt-sysops/native/src/main/scala/org/scalafmt/sysops/PlatformCompat.scala
@@ -7,8 +7,6 @@ private[scalafmt] object PlatformCompat {
   def isScalaNative = true
   def prepareCommand(cmd: Seq[String]) =
     if (Platform.isWindows()) cmd.map(arg => s""""$arg"""") else cmd
-  def fixPathOnNativeWindows(path: String) =
-    if (Platform.isWindows()) path.replace('/', '\\') else path
   def isNativeOnWindows = Platform.isWindows()
   def relativize(base: AbsoluteFile, file: AbsoluteFile) =
     if (Platform.isWindows()) base.path.relativize(file.path)
