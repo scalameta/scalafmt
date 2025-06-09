db9d73896226bdb42ce1c156b156c8f1fc01dbb1
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-06-09 07:02:48 -0700

    fix-3-before-each

--- scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
+++ scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
@@ -154,4 +154,9 @@ class FormatTests extends FunSuite with CanRunTests with FormatAssertions {
       Report.heatmap(debugResults.result()),
     )
   }
+
+  override def beforeEach(context: BeforeEach): Unit = LoggerOps.logger
+    .debug(s"Starting: ${context.test.name}")
+  override def afterEach(context: AfterEach): Unit = LoggerOps.logger
+    .debug(s"Finished: ${context.test.name}")
 }
