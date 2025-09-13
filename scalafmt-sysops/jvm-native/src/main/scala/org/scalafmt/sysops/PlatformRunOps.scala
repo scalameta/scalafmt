3aaf48a8efd56f5d53fc17a94b005f9e1e01ae9f
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-09-12 22:23:32 -0700

    PlatformRunOps: return `Seq[String]` in `runArgv` (#4970)

--- scalafmt-sysops/jvm-native/src/main/scala/org/scalafmt/sysops/PlatformRunOps.scala
+++ scalafmt-sysops/jvm-native/src/main/scala/org/scalafmt/sysops/PlatformRunOps.scala
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContextExecutorService
+import scala.sys.process.Process
 import scala.sys.process.ProcessLogger
 import scala.util.Failure
 import scala.util.Success
@@ -38,21 +39,24 @@ private[scalafmt] object PlatformRunOps {
   implicit def parasiticExecutionContext: ExecutionContext =
     GranularDialectAsyncOps.parasiticExecutionContext
 
-  def runArgv(cmd: Seq[String], cwd: Option[Path]): Try[String] = {
+  def runArgv(cmd: Seq[String], cwd: Option[Path]): Try[Seq[String]] = {
+    val out = Seq.newBuilder[String]
     val err = new StringBuilder()
-    val logger = ProcessLogger(_ => (), x => err.append("\n> ").append(x))
+    val logger = ProcessLogger(out += _, err.append("\n> ").append(_))
     val argv =
       if (PlatformCompat.isNativeOnWindows) cmd.map(arg => '"' + arg + '"')
       else cmd
-    try {
-      val proc = sys.process.Process(argv, cwd.map(_.toFile))
-      Success(proc.!!(logger).trim)
-    } catch {
-      case e: Throwable =>
-        val msg =
-          s"Failed to run '${cmd.mkString(" ")}'. Error:${err.result()}\n"
-        Failure(new IllegalStateException(msg, e))
+    def failed(e: Throwable) = {
+      val msg = cmd
+        .addString(new StringBuilder(), "Failed to run '", " ", "'. Error: ")
+        .append(err).append('\n')
+      Failure(new IllegalStateException(msg.toString(), e))
     }
+    try {
+      val exit = Process(argv, cwd.map(_.toFile)).!(logger)
+      if (exit != 0) failed(new RuntimeException("exit code " + exit))
+      else Success(out.result())
+    } catch { case e: Throwable => failed(e) }
   }
 
   def exit(code: Int): Nothing = sys.exit(code)
