ed8da871d5565e7717c786b23931fcc5befdd974
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-09-14 07:58:55 -0700

    Test multiline strings after aligned tokens

--- scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
+++ scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
@@ -148,7 +148,7 @@ class FormatTests extends FunSuite with CanRunTests with FormatAssertions {
     val explored = Debug.explored.get()
     logger.debug(s"Total explored: $explored")
     if (!onlyUnit && !onlyManual)
-      assertEquals(explored, 2597792, "total explored")
+      assertEquals(explored, 2597948, "total explored")
     if (!sys.env.contains("CI")) PlatformFileOps.writeFileAsync(
       FileOps.getPath("target", "index.html"),
       Report.heatmap(debugResults.result()),
