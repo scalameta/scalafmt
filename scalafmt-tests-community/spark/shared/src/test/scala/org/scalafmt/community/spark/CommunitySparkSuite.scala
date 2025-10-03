b2892840147f90a22b21629b1b325e26ecfd83e4
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-10-03 16:52:44 -0700

    RedundantBraces: keep if after semicolon

--- scalafmt-tests-community/spark/shared/src/test/scala/org/scalafmt/community/spark/CommunitySparkSuite.scala
+++ scalafmt-tests-community/spark/shared/src/test/scala/org/scalafmt/community/spark/CommunitySparkSuite.scala
@@ -9,7 +9,7 @@ abstract class CommunitySparkSuite(name: String)
 
 class CommunitySpark3_4Suite extends CommunitySparkSuite("spark-3.4") {
 
-  override protected def totalStatesVisited: Option[Int] = Some(85923023)
+  override protected def totalStatesVisited: Option[Int] = Some(85922751)
 
   override protected def builds = Seq(getBuild("v3.4.1", dialects.Scala213, 2585))
 
@@ -17,7 +17,7 @@ class CommunitySpark3_4Suite extends CommunitySparkSuite("spark-3.4") {
 
 class CommunitySpark3_5Suite extends CommunitySparkSuite("spark-3.5") {
 
-  override protected def totalStatesVisited: Option[Int] = Some(90889168)
+  override protected def totalStatesVisited: Option[Int] = Some(90888830)
 
   override protected def builds = Seq(getBuild("v3.5.3", dialects.Scala213, 2756))
 
