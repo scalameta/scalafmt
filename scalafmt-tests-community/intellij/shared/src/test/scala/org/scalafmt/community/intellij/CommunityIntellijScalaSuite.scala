7032aeca8ac7bcde873c7c921fd67f5be059dd8b
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-12-04 16:32:27 -0800

    ScalafmtConfig: use `.forMain` if no layout match

--- scalafmt-tests-community/intellij/shared/src/test/scala/org/scalafmt/community/intellij/CommunityIntellijScalaSuite.scala
+++ scalafmt-tests-community/intellij/shared/src/test/scala/org/scalafmt/community/intellij/CommunityIntellijScalaSuite.scala
@@ -13,7 +13,7 @@ abstract class CommunityIntellijScalaSuite(name: String)
 class CommunityIntellijScala_2024_2_Suite
     extends CommunityIntellijScalaSuite("intellij-scala-2024.2") {
 
-  override protected def totalStatesVisited: Option[Int] = Some(59389582)
+  override protected def totalStatesVisited: Option[Int] = Some(59391478)
 
   override protected def builds = Seq {
     getBuild(
@@ -54,7 +54,7 @@ class CommunityIntellijScala_2024_2_Suite
 class CommunityIntellijScala_2024_3_Suite
     extends CommunityIntellijScalaSuite("intellij-scala-2024.3") {
 
-  override protected def totalStatesVisited: Option[Int] = Some(59604358)
+  override protected def totalStatesVisited: Option[Int] = Some(59606254)
 
   override protected def builds = Seq {
     getBuild(
