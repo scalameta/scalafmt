ba6a0a3cc3fde1b032ea75e4fa09d79041638ee0
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-12-23 13:06:52 -0800

    Formatter: upgrade to v3.10.3

--- scalafmt-core/shared/src/main/scala/org/scalafmt/util/MarkdownParser.scala
+++ scalafmt-core/shared/src/main/scala/org/scalafmt/util/MarkdownParser.scala
@@ -25,12 +25,14 @@ private[scalafmt] object MarkdownParser {
       case _ =>
     }
 
-    Success(if (hadFencedParts) {
-      val out = new StringBuilder()
-      parts.foreach(_.renderToString(out))
-      if (out.last != '\n') out.append('\n')
-      out.toString()
-    } else code)
+    Success(
+      if (hadFencedParts) {
+        val out = new StringBuilder()
+        parts.foreach(_.renderToString(out))
+        if (out.last != '\n') out.append('\n')
+        out.toString()
+      } else code,
+    )
   }
 
 }
