8779e77b80f78ca121bdda87aa62e9dbd4ef0c09
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-12-25 06:25:06 -0800

    Test scala.js-style apply

--- scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
+++ scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
@@ -146,7 +146,7 @@ class FormatTests extends FunSuite with CanRunTests with FormatAssertions {
     val explored = Debug.explored.get()
     logger.debug(s"Total explored: $explored")
     if (!onlyUnit && !onlyManual)
-      assertEquals(explored, 2629754, "total explored")
+      assertEquals(explored, 2635184, "total explored")
     if (!sys.env.contains("CI")) PlatformFileOps.writeFileAsync(
       FileOps.getPath("target", "index.html"),
       Report.heatmap(debugResults.result()),
