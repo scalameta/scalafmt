1799d472d5e30d9aac588d68f1f0024d5821c0ab
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2026-01-06 23:26:58 -0800

    FormatOps: NL before single-line infix braces arg

--- scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
+++ scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
@@ -148,7 +148,7 @@ class FormatTests extends FunSuite with CanRunTests with FormatAssertions {
     val explored = Debug.explored.get()
     logger.debug(s"Total explored: $explored")
     if (!onlyUnit && !onlyManual)
-      assertEquals(explored, 2708512, "total explored")
+      assertEquals(explored, 2708518, "total explored")
     if (!sys.env.contains("CI")) PlatformFileOps.writeFileAsync(
       FileOps.getPath("target", "index.html"),
       Report.heatmap(debugResults.result()),
