65a5f0c6a0f8eeb669cc24d15182a8c8846683d8
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2026-08-30 14:42:51 -0700

    Test verticalMultiline lambda by position
    A lambda's parameters land in several different places depending on where
    the lambda sits. Some of it looks wrong.

--- scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
+++ scalafmt-tests/shared/src/test/scala/org/scalafmt/FormatTests.scala
@@ -159,7 +159,7 @@ class FormatTests
     val explored = Debug.explored.get()
     logger.debug(s"Total explored: $explored")
     if (!onlyUnit && !onlyManual)
-      assertEquals(explored, 2615188, "total explored")
+      assertEquals(explored, 2620534, "total explored")
     if (!sys.env.contains("CI")) PlatformFileOps.writeFileAsync(
       FileOps.getPath("target", "index.html"),
       Report.heatmap(debugResults.result()),
