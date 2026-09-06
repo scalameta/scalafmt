432bdeea8ad6703d8894bf9e07ea4d513e6a32ec
Scala Steward <scala_steward@virtuslab.com>
2026-09-06 23:58:55 +0000

    Update scala3-library_sjs1_3 to 3.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import sbt.Keys.*
 object Dependencies {
   val scala212 = "2.12.21"
   val scala213 = "2.13.18"
-  val scala3 = "3.3.8"
+  val scala3 = "3.9.0"
   val scala2Versions = Seq(scala213, scala212)
   val scalaVersions = scala2Versions :+ scala3
 
